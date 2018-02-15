8cefb92719951851f97147549b667297eabc1a17
Adrien Oyono <adrienoyono@gmail.com>
2018-02-15 12:07:23 +0100

    adds option to list available product types

--- eodag/cli.py
+++ eodag/cli.py
@@ -5,9 +5,12 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
 import sys
 
 import click
+import yaml
+import yaml.parser
 
 from eodag.api.core import SatImagesAPI
 from eodag.utils.logging import setup_logging
@@ -33,8 +36,27 @@ click.disable_unicode_literals_warning = True
               help='Control the verbosity of the logs. For maximum verbosity, type -vvv')
 @click.option('-p', '--productType',
               help='The product type to search')
+@click.option('-l', '--list', is_flag=True,
+              help='List supported product types and exit')
 def main(**kwargs):
     setup_logging(**kwargs)
+    if kwargs.pop('list'):
+        def format_system_pt(sys_conf):
+            return 'product_type={product_type};collection={collection}'.format(**sys_conf)
+        with open(os.path.join(os.path.dirname(__file__), 'resources', 'system_conf_default.yml'), 'r') as fh:
+            try:
+                conf = yaml.load(fh)
+                for name, config in conf.items():
+                    if 'search' in config and 'products' in config['search']:
+                        click.echo('Product types available for instance {}:'.format(name))
+                        for eodag_pt, real_pt in config['search']['products'].items():
+                            click.echo('- eodag code: {}\t\tcode on system: {}'.format(
+                                eodag_pt, format_system_pt(real_pt)
+                            ))
+                sys.exit(0)
+            except yaml.parser.ParserError as e:
+                click.echo('Unable to load user configuration file: {}'.format(e))
+                sys.exit(1)
     if kwargs['bbox'] != (None,) * 4:
         rect = kwargs.pop('bbox')
         footprint = {'lonmin': rect[0], 'latmin': rect[1], 'lonmax': rect[2], 'latmax': rect[3]}
