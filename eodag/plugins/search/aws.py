6b6d75aa57a7d782f4c206e64326a43c0ac69896
Clément Cazorla <clement.cazorla@c-s.fr>
2018-04-06 17:12:29 +0200

    [bugfix] Correction d'un erreur dans la creation de l'EOProduct dans AWS search

--- eodag/plugins/search/aws.py
+++ eodag/plugins/search/aws.py
@@ -3,24 +3,17 @@
 # All rights reserved
 from __future__ import absolute_import, print_function, unicode_literals
 
-import datetime
 import logging
 
 import shapely.geometry
-
 
 try:  # PY3
     from urllib.parse import urljoin, urlparse
 except ImportError:  # PY2
     from urlparse import urljoin, urlparse
 
-import requests
-from dateutil.parser import parse as dateparse
-
 from eodag.api.product import EOProduct, EOPRODUCT_PROPERTIES
 from eodag.plugins.search.resto import RestoSearch
-from .base import Search
-
 
 logger = logging.getLogger('eodag.plugins.search.resto')
 
@@ -40,19 +33,19 @@ class AwsSearch(RestoSearch):
             logger.info('Found %s products', len(results['features']))
             logger.debug('Adapting plugin results to eodag product representation')
             for result in results['features']:
-                local_filename = result['id'] + '.zip'
+                local_filename = result['id']
                 ref = result['properties']['title'].split('_')[5]
                 year = result['properties']['completionDate'][0:4]
                 month = str(int(result['properties']['completionDate'][5:7]))
                 day = str(int(result['properties']['completionDate'][8:10]))
 
-                download_url = self.dl_base + '/' + ref[1:3] + '/' + ref[3] + '/' + ref[4:6] + '/' + year + '/' + month + '/' + day + '/' + '0' + '/'
+                download_url = 'tiles' + '/' + ref[1:3] + '/' + ref[3] + '/' + ref[4:6] + '/' + year + '/' + month + '/' + day + '/' + '0' + '/'
 
                 product = EOProduct(
                     result['id'],
                     self.instance_name,
                     download_url,
-                    download_url,
+                    local_filename,
                     shapely.geometry.shape(result['geometry']),
                     search_bbox,
                     # EOPRODUCT_PROPERTIES are based on resto representation of Earth observation products properties
