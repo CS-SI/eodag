2aebe30fefece78cda313db07eac4cd2c9f4cc05
Adrien Oyono <adrienoyono@gmail.com>
2018-06-20 10:31:41 +0200

    fixes bug related to already downloaded products
    Fix #40

--- eodag/plugins/download/http.py
+++ eodag/plugins/download/http.py
@@ -44,15 +44,15 @@ class HTTPDownload(Download):
             download_records = os.path.join(self.config['outputs_prefix'], '.downloaded')
             try:
                 os.makedirs(download_records)
-            except OSError:
-                import traceback as tb
-                logger.warning('Unable to create record directory. Maybe the it already exists. Got:\n%s',
-                               tb.format_exc())
+            except OSError as exc:
+                import errno
+                if exc.errno != errno.EEXIST:   # Skip error if dir exists
+                    import traceback as tb
+                    logger.warning('Unable to create record directory. Got:\n%s', tb.format_exc())
             url_hash = hashlib.md5(url.encode('utf-8')).hexdigest()
             record_filename = os.path.join(download_records, url_hash)
             if os.path.isfile(record_filename) and os.path.isfile(local_file_path):
-                logger.info('Product already downloaded. Retrieve it at %s', local_file_path)
-                return local_file_path
+                return self.__finalize(local_file_path)
             # Remove the record file if local_file_path is absent (e.g. it was deleted while record wasn't)
             elif os.path.isfile(record_filename):
                 logger.debug('Record file found (%s) but not the actual file', record_filename)
@@ -77,32 +77,7 @@ class HTTPDownload(Download):
                     with open(record_filename, 'w') as fh:
                         fh.write(url)
                     logger.debug('Download recorded in %s', record_filename)
-                    if self.config['extract'] and zipfile.is_zipfile(local_file_path):
-                        logger.info('Extraction activated')
-                        with zipfile.ZipFile(local_file_path, 'r') as zfile:
-                            fileinfos = zfile.infolist()
-                            with tqdm(fileinfos, unit='file', desc='Extracting files from {}'.format(
-                                    local_file_path)) as progressbar:
-                                for fileinfo in progressbar:
-                                    zfile.extract(
-                                        fileinfo,
-                                        path=os.path.join(
-                                            self.config['outputs_prefix'],
-                                            local_file_path[:local_file_path.index('.zip')])
-                                    )
-                        # Handle depth levels in the product archive. For example, if the downloaded archive was
-                        # extracted to: /top_level/product_base_dir and archive_depth was configured to 2, the product
-                        # location will be /top_level/product_base_dir.
-                        # WARNING: A strong assumption is made here: there is only one subdirectory per level
-                        archive_depth = self.config.get('archive_depth', 1)
-                        product_path = local_file_path[:local_file_path.index('.zip')]
-                        count = 1
-                        while count < archive_depth:
-                            product_path = os.path.join(product_path, os.listdir(product_path)[0])
-                            count += 1
-                        return product_path
-                    else:
-                        return local_file_path
+                    return self.__finalize(local_file_path)
         else:
             path = product.location.replace('file://', '')
             logger.info('Product already present on this platform. Identifier: %s', path)
@@ -119,3 +94,38 @@ class HTTPDownload(Download):
                 raise RuntimeError('Product {} is incompatible with download plugin {}\n{}'.format(
                     product, self.name, tb.format_exc()
                 ))
+
+    def __finalize(self, local_file_path):
+        """Finalize the download process.
+
+        :param local_file_path: The path to the local zip archive downloaded or already present
+        :type local_file_path: str or unicode
+        :return: the absolute path to the product
+        """
+        if self.config['extract'] and zipfile.is_zipfile(local_file_path):
+            if not os.path.exists(local_file_path[:local_file_path.index('.zip')]):
+                logger.info('Extraction activated')
+                with zipfile.ZipFile(local_file_path, 'r') as zfile:
+                    fileinfos = zfile.infolist()
+                    with tqdm(fileinfos, unit='file', desc='Extracting files from {}'.format(
+                            local_file_path)) as progressbar:
+                        for fileinfo in progressbar:
+                            zfile.extract(
+                                fileinfo,
+                                path=os.path.join(
+                                    self.config['outputs_prefix'],
+                                    local_file_path[:local_file_path.index('.zip')])
+                            )
+            # Handle depth levels in the product archive. For example, if the downloaded archive was
+            # extracted to: /top_level/product_base_dir and archive_depth was configured to 2, the product
+            # location will be /top_level/product_base_dir.
+            # WARNING: A strong assumption is made here: there is only one subdirectory per level
+            archive_depth = self.config.get('archive_depth', 1)
+            product_path = local_file_path[:local_file_path.index('.zip')]
+            count = 1
+            while count < archive_depth:
+                product_path = os.path.join(product_path, os.listdir(product_path)[0])
+                count += 1
+            return product_path
+        else:
+            return local_file_path
