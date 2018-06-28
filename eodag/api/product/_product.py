0936f9af6896a24cfd8a2d889c50ec093e1fbd4e
Clément Cazorla <clement.cazorla@c-s.fr>
2018-06-28 15:21:02 +0200

    correction in get_quicklook, do not download if quicklook is None see #26

--- eodag/api/product/_product.py
+++ eodag/api/product/_product.py
@@ -267,36 +267,36 @@ class EOProduct(object):
         :param filename: Name of the file to be downloaded
         :type filename: class:`str` or `class:`unicode`
         """
+        if self.properties['quicklook'] is not None:
+            if self.provider == 'scihub':
+                auth = tuple(self.downloader.config['credentials'].values())
+            else:
+                auth = self.downloader_auth.authenticate() if self.downloader_auth is not None else self.downloader_auth
 
-        if self.provider == 'scihub':
-            auth = tuple(self.downloader.config['credentials'].values())
-        else:
-            auth = self.downloader_auth.authenticate() if self.downloader_auth is not None else self.downloader_auth
+            local_dir_path = os.path.join(self.downloader.config[u'outputs_prefix'], 'quicklooks')
 
-        local_dir_path = os.path.join(self.downloader.config[u'outputs_prefix'], 'quicklooks')
+            if not os.path.isdir(local_dir_path):
+                os.makedirs(local_dir_path)
 
-        if not os.path.isdir(local_dir_path):
-            os.makedirs(local_dir_path)
+            if filename is None:
+                local_file_path = os.path.join(local_dir_path, self.properties['id'] + '.' +
+                                               self.properties['quicklook'].split('.')[-1])
+            else:
+                local_file_path = os.path.join(local_dir_path, filename)
 
-        if filename is None:
-            local_file_path = os.path.join(local_dir_path, self.properties['id'] + '.' +
-                                           self.properties['quicklook'].split('.')[-1])
-        else:
-            local_file_path = os.path.join(local_dir_path, filename)
-
-        if not os.path.isfile(local_file_path):
-            with requests.get(self.properties['quicklook'], stream=True, auth=auth) as stream:
-                try:
-                    stream.raise_for_status()
-                except HTTPError:
-                    import traceback as tb
-                    logger.error("Error while getting resource :\n%s", tb.format_exc())
-                else:
-                    stream_size = int(stream.headers.get('content-length', 0))
-                    with open(local_file_path, 'wb') as fhandle:
-                        progressbar = tqdm(total=stream_size, unit='KB', unit_scale=True)
-                        for chunk in stream.iter_content(chunk_size=64 * 1024):
-                            if chunk:
-                                progressbar.update(len(chunk))
-                                fhandle.write(chunk)
-                    logger.info('Download recorded in %s', local_dir_path)
+            if not os.path.isfile(local_file_path):
+                with requests.get(self.properties['quicklook'], stream=True, auth=auth) as stream:
+                    try:
+                        stream.raise_for_status()
+                    except HTTPError:
+                        import traceback as tb
+                        logger.error("Error while getting resource :\n%s", tb.format_exc())
+                    else:
+                        stream_size = int(stream.headers.get('content-length', 0))
+                        with open(local_file_path, 'wb') as fhandle:
+                            progressbar = tqdm(total=stream_size, unit='KB', unit_scale=True)
+                            for chunk in stream.iter_content(chunk_size=64 * 1024):
+                                if chunk:
+                                    progressbar.update(len(chunk))
+                                    fhandle.write(chunk)
+                        logger.info('Download recorded in %s', local_dir_path)
