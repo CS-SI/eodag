4611eb3b49fbc45849f6452cc5067690b09e7adb
Adrien Oyono <adrien.oyono@c-s.fr>
2019-04-16 15:09:38 +0200

    removes pinned dependancy for eodag rest service

--- setup.py
+++ setup.py
@@ -61,9 +61,9 @@ setup(
         'jsonpath-rw',
         'lxml',
         'xarray',
-        'flask == 1.0.2',
-        'jinja2 == 2.10',
-        'markdown == 3.0.1',
+        'flask >= 1.0.2',
+        'jinja2 >= 2.10',
+        'markdown >= 3.0.1',
         'unidecode == 1.0.22',
         # Temporary to avoid rasterio error on importing mock (to be removed when it is fixed upstream)
         'mock; python_version < "3.5" ',
