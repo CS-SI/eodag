d24887271313fc4e73340357bb93286e078f828d
Adrien Oyono <adrien.oyono@c-s.fr>
2019-04-19 11:10:58 +0200

    applies black code style to product module and usgs and csw plugins

--- eodag/plugins/apis/usgs.py
+++ eodag/plugins/apis/usgs.py
@@ -31,95 +31,138 @@ from usgs import USGSError, api
 
 from eodag.api.product import EOProduct
 from eodag.api.product.metadata_mapping import properties_from_json
-from .base import Api
+from eodag.plugins.apis.base import Api
 
-
-logger = logging.getLogger('eodag.plugins.apis.usgs')
+logger = logging.getLogger("eodag.plugins.apis.usgs")
 
 
 class UsgsApi(Api):
+    """A plugin that enables to query and download data on the USGS catalogues"""
 
     def query(self, product_type, **kwargs):
-        api.login(self.config.credentials['username'], self.config.credentials['password'], save=True)
-        usgs_dataset = self.config.products[product_type]['dataset']
-        usgs_catalog_node = self.config.products[product_type]['catalog_node']
-        start_date = kwargs.pop('startTimeFromAscendingNode', None)
-        end_date = kwargs.pop('completionTimeFromAscendingNode', None)
-        footprint = kwargs.pop('geometry', None)
+        """Search for data on USGS catalogues"""
+        api.login(
+            self.config.credentials["username"],
+            self.config.credentials["password"],
+            save=True,
+        )
+        usgs_dataset = self.config.products[product_type]["dataset"]
+        usgs_catalog_node = self.config.products[product_type]["catalog_node"]
+        start_date = kwargs.pop("startTimeFromAscendingNode", None)
+        end_date = kwargs.pop("completionTimeFromAscendingNode", None)
+        footprint = kwargs.pop("geometry", None)
 
         # Configuration to generate the download url of search results
         result_summary_pattern = re.compile(
-            r'^Entity ID: .+, Acquisition Date: .+, Path: (?P<path>\d+), Row: (?P<row>\d+)$')
-        # See https://pyformat.info/, on section "Padding and aligning strings" to understand {path:0>3} and {row:0>3}.
-        # It roughly means: 'if the string that will be passed as "path" has length < 3, prepend as much "0"s as needed
-        # to reach length 3' and same for "row"
-        dl_url_pattern = '{base_url}/L8/{path:0>3}/{row:0>3}/{entity}.tar.bz'
+            r"^Entity ID: .+, Acquisition Date: .+, Path: (?P<path>\d+), Row: (?P<row>\d+)$"  # noqa
+        )
+        # See https://pyformat.info/, on section "Padding and aligning strings" to
+        # understand {path:0>3} and {row:0>3}.
+        # It roughly means: 'if the string that will be passed as "path" has length < 3,
+        # prepend as much "0"s as needed to reach length 3' and same for "row"
+        dl_url_pattern = "{base_url}/L8/{path:0>3}/{row:0>3}/{entity}.tar.bz"
 
         final = []
         if footprint and len(footprint.keys()) == 4:  # a rectangle (or bbox)
-            lower_left = {'longitude': footprint['lonmin'], 'latitude': footprint['latmin']}
-            upper_right = {'longitude': footprint['lonmax'], 'latitude': footprint['latmax']}
+            lower_left = {
+                "longitude": footprint["lonmin"],
+                "latitude": footprint["latmin"],
+            }
+            upper_right = {
+                "longitude": footprint["lonmax"],
+                "latitude": footprint["latmax"],
+            }
         else:
             lower_left, upper_right = None, None
         try:
-            results = api.search(usgs_dataset, usgs_catalog_node, start_date=start_date, end_date=end_date,
-                                 ll=lower_left, ur=upper_right)
+            results = api.search(
+                usgs_dataset,
+                usgs_catalog_node,
+                start_date=start_date,
+                end_date=end_date,
+                ll=lower_left,
+                ur=upper_right,
+            )
 
-            for result in results['data']['results']:
-                r_lower_left = result['lowerLeftCoordinate']
-                r_upper_right = result['upperRightCoordinate']
-                summary_match = result_summary_pattern.match(result['summary']).groupdict()
-                result['geometry'] = geometry.box(
-                    r_lower_left['longitude'], r_lower_left['latitude'],
-                    r_upper_right['longitude'], r_upper_right['latitude']
+            for result in results["data"]["results"]:
+                r_lower_left = result["lowerLeftCoordinate"]
+                r_upper_right = result["upperRightCoordinate"]
+                summary_match = result_summary_pattern.match(
+                    result["summary"]
+                ).groupdict()
+                result["geometry"] = geometry.box(
+                    r_lower_left["longitude"],
+                    r_lower_left["latitude"],
+                    r_upper_right["longitude"],
+                    r_upper_right["latitude"],
                 )
-                result['productType'] = usgs_dataset
-                final.append(EOProduct(
-                    product_type,
-                    self.provider,
-                    dl_url_pattern.format(base_url='file://')
-                    if self.config.product_location_scheme == 'file'
-                    else dl_url_pattern.format(
-                        base_url=self.config.google_base_url.rstrip('/'),
-                        entity=result['entityId'],
-                        **summary_match
-                    ),
-                    properties_from_json(result, self.config.metadata_mapping),
-                    searched_bbox=footprint,
-                ))
+                result["productType"] = usgs_dataset
+                final.append(
+                    EOProduct(
+                        product_type,
+                        self.provider,
+                        dl_url_pattern.format(base_url="file://")
+                        if self.config.product_location_scheme == "file"
+                        else dl_url_pattern.format(
+                            base_url=self.config.google_base_url.rstrip("/"),
+                            entity=result["entityId"],
+                            **summary_match
+                        ),
+                        properties_from_json(result, self.config.metadata_mapping),
+                        searched_bbox=footprint,
+                    )
+                )
         except USGSError as e:
-            logger.debug('Product type %s does not exist on catalogue %s', usgs_dataset, usgs_catalog_node)
+            logger.debug(
+                "Product type %s does not exist on catalogue %s",
+                usgs_dataset,
+                usgs_catalog_node,
+            )
             logger.debug("Skipping error: %s", e)
         api.logout()
         return final, len(final)
 
     def download(self, product, auth=None, progress_callback=None):
+        """Download data from USGS catalogues"""
         url = product.remote_location
         if not url:
-            logger.debug('Unable to get download url for %s, skipping download', product)
+            logger.debug(
+                "Unable to get download url for %s, skipping download", product
+            )
             return
-        logger.info('Download url: %s', url)
+        logger.info("Download url: %s", url)
 
-        filename = product.properties['title'] + '.tar.bz'
+        filename = product.properties["title"] + ".tar.bz"
         local_file_path = os.path.join(self.config.outputs_prefix, filename)
-        download_records = os.path.join(self.config.outputs_prefix, '.downloaded')
+        download_records = os.path.join(self.config.outputs_prefix, ".downloaded")
         if not os.path.exists(download_records):
             os.makedirs(download_records)
-        url_hash = hashlib.md5(url.encode('utf-8')).hexdigest()
+        url_hash = hashlib.md5(url.encode("utf-8")).hexdigest()
         record_filename = os.path.join(download_records, url_hash)
         if os.path.isfile(record_filename) and os.path.isfile(local_file_path):
-            logger.info('Product already downloaded. Retrieve it at %s', local_file_path)
+            logger.info(
+                "Product already downloaded. Retrieve it at %s", local_file_path
+            )
             return local_file_path
-        # Remove the record file if local_file_path is absent (e.g. it was deleted while record wasn't)
+        # Remove the record file if local_file_path is absent (e.g. it was deleted
+        # while record wasn't)
         elif os.path.isfile(record_filename):
-            logger.debug('Record file found (%s) but not the actual file', record_filename)
-            logger.debug('Removing record file : %s', record_filename)
+            logger.debug(
+                "Record file found (%s) but not the actual file", record_filename
+            )
+            logger.debug("Removing record file : %s", record_filename)
             os.remove(record_filename)
 
-        with requests.get(url, stream=True, auth=auth, params=getattr(self.config, 'dl_url_params', {}), verify=False,
-                          hooks={'response': lambda r, *args, **kwargs: print('\n', r.url)}) as stream:
-            stream_size = int(stream.headers.get('content-length', 0))
-            with open(local_file_path, 'wb') as fhandle:
+        with requests.get(
+            url,
+            stream=True,
+            auth=auth,
+            params=getattr(self.config, "dl_url_params", {}),
+            verify=False,
+            hooks={"response": lambda r, *args, **kwargs: print("\n", r.url)},
+        ) as stream:
+            stream_size = int(stream.headers.get("content-length", 0))
+            with open(local_file_path, "wb") as fhandle:
                 for chunk in stream.iter_content(chunk_size=64 * 1024):
                     if chunk:
                         fhandle.write(chunk)
@@ -128,19 +171,27 @@ class UsgsApi(Api):
                 stream.raise_for_status()
             except HTTPError:
                 import traceback
-                logger.error("Error while getting resource : %s", traceback.format_exc())
+
+                logger.error(
+                    "Error while getting resource : %s", traceback.format_exc()
+                )
             else:
-                with open(record_filename, 'w') as fh:
+                with open(record_filename, "w") as fh:
                     fh.write(url)
-                logger.debug('Download recorded in %s', record_filename)
+                logger.debug("Download recorded in %s", record_filename)
                 if self.config.extract and zipfile.is_zipfile(local_file_path):
-                    logger.info('Extraction activated')
-                    with zipfile.ZipFile(local_file_path, 'r') as zfile:
+                    logger.info("Extraction activated")
+                    with zipfile.ZipFile(local_file_path, "r") as zfile:
                         fileinfos = zfile.infolist()
-                        with tqdm(fileinfos, unit='file',
-                                  desc='Extracting files from {}'.format(local_file_path)) as progressbar:
+                        with tqdm(
+                            fileinfos,
+                            unit="file",
+                            desc="Extracting files from {}".format(local_file_path),
+                        ) as progressbar:
                             for fileinfo in progressbar:
-                                zfile.extract(fileinfo, path=self.config['outputs_prefix'])
-                    return local_file_path[:local_file_path.index('.tar.bz')]
+                                zfile.extract(
+                                    fileinfo, path=self.config["outputs_prefix"]
+                                )
+                    return local_file_path[: local_file_path.index(".tar.bz")]
                 else:
                     return local_file_path
--- eodag/plugins/search/csw.py
+++ eodag/plugins/search/csw.py
@@ -23,7 +23,11 @@ import re
 import pyproj
 from owslib.csw import CatalogueServiceWeb
 from owslib.fes import (
-    BBox, PropertyIsEqualTo, PropertyIsGreaterThanOrEqualTo, PropertyIsLessThanOrEqualTo, PropertyIsLike,
+    BBox,
+    PropertyIsEqualTo,
+    PropertyIsGreaterThanOrEqualTo,
+    PropertyIsLessThanOrEqualTo,
+    PropertyIsLike,
 )
 from owslib.ows import ExceptionReport
 from shapely import geometry, wkt
@@ -34,124 +38,168 @@ from eodag.plugins.search.base import Search
 from eodag.utils import DEFAULT_PROJ
 from eodag.utils.import_system import patch_owslib_requests
 
+logger = logging.getLogger("eodag.plugins.search.csw")
 
-logger = logging.getLogger('eodag.plugins.search.csw')
-
-SUPPORTED_REFERENCE_SCHEMES = [
-    'WWW:DOWNLOAD-1.0-http--download',
-]
+SUPPORTED_REFERENCE_SCHEMES = ["WWW:DOWNLOAD-1.0-http--download"]
 
 
 class CSWSearch(Search):
+    """A plugin for implementing search based on OGC CSW"""
 
     def __init__(self, provider, config):
         super(CSWSearch, self).__init__(provider, config)
         self.catalog = None
 
     def query(self, product_type, auth=None, **kwargs):
+        """Perform a search on a OGC/CSW-like interface"""
         if auth is not None:
-            self.__init_catalog(**getattr(auth.config, 'credentials', {}))
+            self.__init_catalog(**getattr(auth.config, "credentials", {}))
         else:
             self.__init_catalog()
         results = []
         if self.catalog:
-            provider_product_type = self.config.products[product_type]['product_type']
-            for product_type_def in self.config.search_definition['product_type_tags']:
-                product_type_search_tag = product_type_def['name']
-                logger.debug('Querying <%s> tag for product type %s', product_type_search_tag, provider_product_type)
-                constraints = self.__convert_query_params(product_type_def, provider_product_type, kwargs)
+            provider_product_type = self.config.products[product_type]["product_type"]
+            for product_type_def in self.config.search_definition["product_type_tags"]:
+                product_type_search_tag = product_type_def["name"]
+                logger.debug(
+                    "Querying <%s> tag for product type %s",
+                    product_type_search_tag,
+                    provider_product_type,
+                )
+                constraints = self.__convert_query_params(
+                    product_type_def, provider_product_type, kwargs
+                )
                 with patch_owslib_requests(verify=True):
                     try:
-                        self.catalog.getrecords2(constraints=constraints, esn='full', maxrecords=10)
+                        self.catalog.getrecords2(
+                            constraints=constraints, esn="full", maxrecords=10
+                        )
                     except ExceptionReport:
                         import traceback as tb
-                        logger.warning('Failed to query %s for product type %s : %s', product_type_search_tag,
-                                       product_type, tb.format_exc())
+
+                        logger.warning(
+                            "Failed to query %s for product type %s : %s",
+                            product_type_search_tag,
+                            product_type,
+                            tb.format_exc(),
+                        )
                         continue
                 partial_results = [
-                    self.__build_product(record, product_type, **kwargs) for record in self.catalog.records.values()]
-                logger.info('Found %s results querying %s', len(partial_results), product_type_search_tag)
+                    self.__build_product(record, product_type, **kwargs)
+                    for record in self.catalog.records.values()
+                ]
+                logger.info(
+                    "Found %s results querying %s",
+                    len(partial_results),
+                    product_type_search_tag,
+                )
                 results.extend(partial_results)
-        logger.info('Found %s overall results', len(results))
+        logger.info("Found %s overall results", len(results))
         return results, len(results)
 
     def __init_catalog(self, username=None, password=None):
         """Initializes a catalogue by performing a GetCapabilities request on the url"""
         if not self.catalog:
             api_endpoint = self.config.api_endpoint
-            version = getattr(self.config, 'version', '2.0.2')
-            logger.debug('Initialising CSW catalog at %s', api_endpoint)
+            version = getattr(self.config, "version", "2.0.2")
+            logger.debug("Initialising CSW catalog at %s", api_endpoint)
             with patch_owslib_requests(verify=True):
                 try:
                     self.catalog = CatalogueServiceWeb(
                         api_endpoint,
                         version=version,
                         username=username,
-                        password=password
+                        password=password,
                     )
                 except Exception as e:
-                    logger.warning('Initialization of catalog failed due to error: (%s: %s)', type(e), e)
+                    logger.warning(
+                        "Initialization of catalog failed due to error: (%s: %s)",
+                        type(e),
+                        e,
+                    )
 
     def __build_product(self, rec, product_type, **kwargs):
         """Enable search results to be handled by http download plugin"""
-        download_url = ''
-        resource_filter = re.compile(self.config.search_definition.get('resource_location_filter', ''))
+        download_url = ""
+        resource_filter = re.compile(
+            self.config.search_definition.get("resource_location_filter", "")
+        )
         for ref in rec.references:
-            if ref['scheme'] in SUPPORTED_REFERENCE_SCHEMES:
-                if resource_filter.pattern and resource_filter.search(ref['url']):
-                    download_url = ref['url']
+            if ref["scheme"] in SUPPORTED_REFERENCE_SCHEMES:
+                if resource_filter.pattern and resource_filter.search(ref["url"]):
+                    download_url = ref["url"]
                 else:
-                    download_url = ref['url']
+                    download_url = ref["url"]
                 break
         properties = properties_from_xml(rec.xml, self.config.metadata_mapping)
-        if not properties['geometry']:
+        if not properties["geometry"]:
             bbox = rec.bbox_wgs84
             if not bbox:
-                code = 'EPSG:4326'
+                code = "EPSG:4326"
                 if rec.bbox.crs and rec.bbox.crs.code and rec.bbox.crs.code > 0:
-                    code = ':'.join((str(rec.bbox.crs.id), str(rec.bbox.crs.code)))
+                    code = ":".join((str(rec.bbox.crs.id), str(rec.bbox.crs.code)))
                 rec_proj = pyproj.Proj(init=code)
                 default_proj_as_pyproj = pyproj.Proj(DEFAULT_PROJ)
-                maxx, maxy = pyproj.transform(rec_proj, default_proj_as_pyproj, rec.bbox.maxx, rec.bbox.maxy)
-                minx, miny = pyproj.transform(rec_proj, default_proj_as_pyproj, rec.bbox.minx, rec.bbox.miny)
+                maxx, maxy = pyproj.transform(
+                    rec_proj, default_proj_as_pyproj, rec.bbox.maxx, rec.bbox.maxy
+                )
+                minx, miny = pyproj.transform(
+                    rec_proj, default_proj_as_pyproj, rec.bbox.minx, rec.bbox.miny
+                )
                 bbox = (minx, miny, maxx, maxy)
-            properties['geometry'] = geometry.box(*bbox)
-        # Ensure the geometry property is shapely-compatible (the geometry is assumed to be a wkt)
+            properties["geometry"] = geometry.box(*bbox)
+        # Ensure the geometry property is shapely-compatible (the geometry is assumed
+        # to be a wkt)
         else:
-            properties['geometry'] = wkt.loads(properties['geometry'])
+            properties["geometry"] = wkt.loads(properties["geometry"])
         return EOProduct(
             product_type,
             self.provider,
             download_url,
             properties,
-            searched_bbox=kwargs.get('footprints'),
+            searched_bbox=kwargs.get("footprints"),
         )
 
     def __convert_query_params(self, product_type_def, product_type, params):
         """Translates eodag search to CSW constraints using owslib constraint classes"""
         constraints = []
-        # How the match should be performed (fuzzy, prefix, postfix or exact). defaults to fuzzy
-        pt_tag, matching = product_type_def['name'], product_type_def.get('matching', 'fuzzy')
-        if matching == 'prefix':
-            constraints.append(PropertyIsLike(pt_tag, '{}%'.format(product_type)))
-        elif matching == 'postfix':
-            constraints.append(PropertyIsLike(pt_tag, '%{}'.format(product_type)))
-        elif matching == 'exact':
+        # How the match should be performed (fuzzy, prefix, postfix or exact).
+        # defaults to fuzzy
+        pt_tag, matching = (
+            product_type_def["name"],
+            product_type_def.get("matching", "fuzzy"),
+        )
+        if matching == "prefix":
+            constraints.append(PropertyIsLike(pt_tag, "{}%".format(product_type)))
+        elif matching == "postfix":
+            constraints.append(PropertyIsLike(pt_tag, "%{}".format(product_type)))
+        elif matching == "exact":
             constraints.append(PropertyIsEqualTo(pt_tag, product_type))
         else:  # unknown matching is considered to be equal to 'fuzzy'
-            constraints.append(PropertyIsLike(pt_tag, '%{}%'.format(product_type)))
+            constraints.append(PropertyIsLike(pt_tag, "%{}%".format(product_type)))
 
         # footprint
-        fp = params.get('geometry')
+        fp = params.get("geometry")
         if fp:
-            constraints.append(BBox([fp['lonmin'], fp['latmin'], fp['lonmax'], fp['latmax']]))
+            constraints.append(
+                BBox([fp["lonmin"], fp["latmin"], fp["lonmax"], fp["latmax"]])
+            )
 
         # dates
-        start, end = params.get('startTimeFromAscendingNode'), params.get('completionTimeFromAscendingNode')
-        if start and 'date_tags' in self.config.search_definition:
-            constraints.append(PropertyIsGreaterThanOrEqualTo(
-                self.config.search_definition['date_tags']['start'], start))
-        if end and 'date_tags' in self.config.search_definition:
-            constraints.append(PropertyIsLessThanOrEqualTo(
-                self.config.search_definition['date_tags']['end'], end))
+        start, end = (
+            params.get("startTimeFromAscendingNode"),
+            params.get("completionTimeFromAscendingNode"),
+        )
+        if start and "date_tags" in self.config.search_definition:
+            constraints.append(
+                PropertyIsGreaterThanOrEqualTo(
+                    self.config.search_definition["date_tags"]["start"], start
+                )
+            )
+        if end and "date_tags" in self.config.search_definition:
+            constraints.append(
+                PropertyIsLessThanOrEqualTo(
+                    self.config.search_definition["date_tags"]["end"], end
+                )
+            )
         return [constraints] if len(constraints) > 1 else constraints
