30cae316ad93ce8ee4ccbf9bb8b9b1bf9e9875f5
Sylvain Brunato <sylvain.brunato@c-s.fr>
2020-02-21 17:13:25 +0100

    doc: Eodag logo added and other minor changes

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,13 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.realpath(__file__))))
 metadata = {}
-with open(os.path.join(BASEDIR, 'eodag', '__meta__.py'), 'r') as f:
+with open(os.path.join(BASEDIR, "eodag", "__meta__.py"), "r") as f:
     exec(f.read(), metadata)
 
 
@@ -33,56 +34,63 @@ with open(os.path.join(BASEDIR, 'eodag', '__meta__.py'), 'r') as f:
 # This is a workaround for versions of sphinx < 1.8 (still in beta development)
 # In Sphinx 1.8.0+ this method is dropped and usage of html_js_files option is preferred
 def setup(app):
-    app.add_javascript("https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js")
-    app.add_javascript("https://unpkg.com/@jupyter-widgets/html-manager@*/dist/embed-amd.js")
+    """
+    setup
+    """
+    app.add_javascript(
+        "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"
+    )
+    app.add_javascript(
+        "https://unpkg.com/@jupyter-widgets/html-manager@*/dist/embed-amd.js"
+    )
 
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.8.0'
+needs_sphinx = "1.8.0"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'nbsphinx',
-    'nbsphinx_link',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "nbsphinx_link",
 ]
 
 # Notebook integration parameters
-nbsphinx_execute = 'auto'
+nbsphinx_execute = "auto"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = metadata['__title__']
-copyright = metadata['__copyright__']
-author = metadata['__author__']
+project = metadata["__title__"]
+copyright = metadata["__copyright__"]
+author = metadata["__author__"]
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = metadata['__version__']
+version = metadata["__version__"]
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -96,10 +104,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -110,24 +118,28 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'logo': 'logo_cs.png',
-    'logo_name': True,
-    'show_related': True,
-    'fixed_sidebar': True,
-    'show_powered_by': False,
+    "logo": "eodag_logo.png",
+    "logo_name": False,
+    "show_related": True,
+    "fixed_sidebar": True,
+    "show_powered_by": False,
 }
+
+# html_logo = 'https://img.shields.io/badge/style-git%20repo-blue?logo=bitbucket&label=bitbucket'
+# .. image:: https://badge.fury.io/py/eodag.svg
+#    :target: https://badge.fury.io/py/eodag
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -135,11 +147,13 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
+    # "index": ["sidebarxtra.html"],
+    "**": [
+        "about.html",
+        "sidebarxtra.html",
+        "navigation.html",
+        "relations.html",  # needs 'show_related': True theme option to display
+        "searchbox.html",
     ]
 }
 
@@ -149,7 +163,7 @@ html_show_sourcelink = False
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'eodagdoc'
+htmlhelp_basename = "eodagdoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -158,15 +172,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -176,8 +187,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'eodag.tex', 'eodag Documentation',
-     'CS Systemes d\'Information (CSSI)', 'manual'),
+    (
+        master_doc,
+        "eodag.tex",
+        "eodag Documentation",
+        "CS Systemes d'Information (CSSI)",
+        "manual",
+    )
 ]
 
 
@@ -185,10 +201,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'eodag', 'eodag Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "eodag", "eodag Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -197,19 +210,25 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'eodag', 'eodag Documentation',
-     author, 'eodag', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "eodag",
+        "eodag Documentation",
+        author,
+        "eodag",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
-    'http://docs.scipy.org/doc/numpy/': None,
-    'http://rasterio.readthedocs.io/en/latest/': None,
-    'http://click.pocoo.org/5/': None,
-    'http://docs.python-requests.org/en/master/': None,
-    'https://shapely.readthedocs.io/en/latest/': None,
-    'https://geopython.github.io/OWSLib/': None,
-    'https://xarray.pydata.org/en/stable/': None,
+    "https://docs.python.org/": None,
+    "http://docs.scipy.org/doc/numpy/": None,
+    "http://rasterio.readthedocs.io/en/latest/": None,
+    "http://click.pocoo.org/5/": None,
+    "http://docs.python-requests.org/en/master/": None,
+    "https://shapely.readthedocs.io/en/latest/": None,
+    "https://geopython.github.io/OWSLib/": None,
+    "https://xarray.pydata.org/en/stable/": None,
 }
--- docs/index.rst
+++ docs/index.rst
@@ -3,11 +3,25 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to eodag's documentation!
-=================================
+.. role:: raw-html(raw)
+    :format: html
 
-The brand new Earth Observation Data Access Gateway:
-https://bitbucket.org/geostorm/eodag
+eodag - documentation
+=====================
+
+Earth Observation Data Access Gateway
+
+:raw-html:`<a class="reference external image-reference" href="https://badge.fury.io/py/eodag" rel="nofollow"><img src="https://badge.fury.io/py/eodag.svg" type="image/svg+xml"></a>`
+:raw-html:`<a class="reference external image-reference" href="https://eodag.readthedocs.io/en/latest/" rel="nofollow"><img alt="https://readthedocs.org/projects/pip/badge/?version=latest&amp;style=flat" src="https://readthedocs.org/projects/pip/badge/?version=latest&amp;style=flat"></a>`
+:raw-html:`<a class="reference external image-reference" href="https://bitbucket.org/geostorm/eodag/issues?status=new&amp;status=open" rel="nofollow"><img src="https://img.shields.io/bitbucket/issues/geostorm/eodag.svg" type="image/svg+xml"></a>`
+:raw-html:`<a class="reference external image-reference" href="https://mybinder.org/v2/git/https%3A%2F%2Fbitbucket.org%2Fgeostorm%2Feodag.git/master?filepath=examples%2Ftuto_basics.ipynb" rel="nofollow"><img src="https://mybinder.org/badge_logo.svg" type="image/svg+xml"></a>`
+:raw-html:`<br>`
+:raw-html:`<a class="reference external image-reference" href="https://pypi.org/project/eodag/" rel="nofollow"><img src="https://img.shields.io/pypi/l/eodag.svg" type="image/svg+xml"></a>`
+:raw-html:`<a class="reference external image-reference" href="https://pypi.org/project/eodag/" rel="nofollow"><img src="https://img.shields.io/pypi/pyversions/eodag.svg" type="image/svg+xml"></a>`
+
+Command line tool and a plugin-oriented Python framework for searching,
+aggregating results and downloading remote sensed images while offering a unified API for data access regardless of the
+data provider.
 
 -------------------------------------------------------------------------------
 
