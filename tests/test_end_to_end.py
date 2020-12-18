614075f279cebe61c79aca7355f2653efeafda94
Sylvain Brunato <sylvain.brunato@c-s.fr>
2020-12-18 11:22:30 +0100

    fix: e2e test mundi extent

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -221,7 +221,12 @@ class TestEODagEndToEnd(unittest.TestCase):
             "S2_MSI_L1C",
             "2019-11-08",
             "2019-11-16",
-            (137.772897, 13.134202, 153.749135, 23.885986),
+            [
+                0.2563590566012408,
+                43.19555008715042,
+                2.379835675499976,
+                43.907759172380565,
+            ],
         )
         expected_filename = "{}".format(product.properties["title"])
         self.execute_download(product, expected_filename)
