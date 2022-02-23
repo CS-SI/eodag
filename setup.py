2d8cec2f6c3a1d7d23c6df6709cb0e3f18780548
Sylvain Brunato <61419125+sbrunato@users.noreply.github.com>
2022-02-23 20:15:01 +0100

    feat: add support for py310 (#407)

--- setup.py
+++ setup.py
@@ -151,6 +151,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
         "Topic :: Scientific/Engineering :: GIS",
