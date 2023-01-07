79172a301a92754d238ad64b4a2f9284cd3133f2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 15:31:42 +0000

    Adding tarfile member sanitization to extractall()

--- eodag/plugins/download/base.py
+++ eodag/plugins/download/base.py
@@ -338,7 +338,26 @@ class Download(PluginTopic):
             elif fs_path.endswith(".tar.gz"):
                 with tarfile.open(fs_path, "r:gz") as zfile:
                     progress_callback.reset(total=1)
-                    zfile.extractall(path=extraction_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner) 
+                        
+                    
+                    safe_extract(zfile, path=extraction_dir)
                     progress_callback(1)
                 shutil.move(extraction_dir, outputs_dir)
             else:
