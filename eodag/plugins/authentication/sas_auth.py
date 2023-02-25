a53035c806553cedb09ab59c827472e85dadc326
Augustin Nesson <augustin.nesson@csgroup.eu>
2023-02-25 14:52:49 +0100

    feat: add Planetary Computer as a new provider

---
+++ eodag/plugins/authentication/sas_auth.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023, CS GROUP - France, https://www.csgroup.eu/
+#
+# This file is part of EODAG project
+#     https://www.github.com/CS-SI/EODAG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+from json import JSONDecodeError
+
+import requests
+from requests.auth import AuthBase
+
+from eodag.plugins.authentication.base import Authentication
+from eodag.utils import USER_AGENT, deepcopy, format_dict_items
+from eodag.utils.exceptions import AuthenticationError
+from eodag.utils.stac_reader import HTTP_REQ_TIMEOUT
+
+logger = logging.getLogger("eodag.plugins.auth.sas_auth")
+
+
+class RequestsSASAuth(AuthBase):
+    """A custom authentication class to be used with requests module"""
+
+    def __init__(self, auth_uri, signed_url_key, headers=None):
+        self.auth_uri = auth_uri
+        self.signed_url_key = signed_url_key
+        self.headers = headers
+        self.signed_urls = {}
+
+    def __call__(self, request):
+        """Perform the actual authentication"""
+
+        # update headers
+        if self.headers and isinstance(self.headers, dict):
+            for k, v in self.headers.items():
+                request.headers[k] = v
+
+        # request the signed_url
+        req_signed_url = self.auth_uri.format(url=request.url)
+        if req_signed_url not in self.signed_urls.keys():
+            logger.debug(f"Signed URL request: {req_signed_url}")
+            try:
+                response = requests.get(
+                    req_signed_url, headers=self.headers, timeout=HTTP_REQ_TIMEOUT
+                )
+                response.raise_for_status()
+                signed_url = response.json().get(self.signed_url_key)
+            except (requests.RequestException, JSONDecodeError, KeyError) as e:
+                raise AuthenticationError(f"Could no get signed url: {str(e)}")
+            else:
+                self.signed_urls[req_signed_url] = signed_url
+
+        request.url = self.signed_urls[req_signed_url]
+
+        return request
+
+
+class SASAuth(Authentication):
+    """SASAuth authentication plugin"""
+
+    def validate_config_credentials(self):
+        """Validate configured credentials"""
+        # credentials are optionnal
+        pass
+
+    def authenticate(self):
+        """Authenticate"""
+        self.validate_config_credentials()
+
+        headers = deepcopy(USER_AGENT)
+
+        # update headers with subscription key if exists
+        apikey = getattr(self.config, "credentials", {}).get("apikey", None)
+        if apikey:
+            headers_update = format_dict_items(self.config.headers, apikey=apikey)
+            headers.update(headers_update)
+
+        return RequestsSASAuth(
+            auth_uri=self.config.auth_uri,
+            signed_url_key=self.config.signed_url_key,
+            headers=headers,
+        )
--- tests/units/test_auth_plugins.py
+++ tests/units/test_auth_plugins.py
@@ -272,3 +272,122 @@ class TestAuthPluginHttpQueryStringAuth(BaseAuthPluginTest):
             AuthenticationError,
             auth_plugin.authenticate,
         )
+
+
+class TestAuthPluginSASAuth(BaseAuthPluginTest):
+    @classmethod
+    def setUpClass(cls):
+        super(TestAuthPluginSASAuth, cls).setUpClass()
+        override_config_from_mapping(
+            cls.providers_config,
+            {
+                "foo_provider": {
+                    "products": {},
+                    "auth": {
+                        "type": "SASAuth",
+                        "auth_uri": "http://foo.bar?href={url}",
+                        "signed_url_key": "href",
+                        "headers": {
+                            "Ocp-Apim-Subscription-Key": "{apikey}",
+                        },
+                    },
+                },
+            },
+        )
+        cls.plugins_manager = PluginManager(cls.providers_config)
+
+    def test_plugins_auth_sasauth_validate_credentials_ok(self):
+        """SASAuth.validate_credentials must be ok on empty or non-empty credentials"""
+        auth_plugin = self.get_auth_plugin("foo_provider")
+
+        auth_plugin.config.credentials = {}
+        auth_plugin.validate_config_credentials()
+        auth_plugin.config.credentials = {"apikey": "foo"}
+        auth_plugin.validate_config_credentials()
+
+    @mock.patch("eodag.plugins.authentication.sas_auth.requests.get", autospec=True)
+    def test_plugins_auth_sasauth_text_token_authenticate_with_credentials(
+        self, mock_requests_get
+    ):
+        """When a user has credentials, SASAuth.authenticate must return
+        a RequestsSASAuth object with his subscription key in its headers
+        """
+        auth_plugin = self.get_auth_plugin("foo_provider")
+
+        auth_plugin.config.credentials = {"apikey": "foo"}
+
+        # mock full signed url get request response
+        mock_requests_get.return_value = mock.Mock()
+        mock_requests_get.return_value.json.return_value = {
+            "msft:expiry": "this_is_test_key_expiration_date",
+            "href": "this_is_test_full_signed_url",
+        }
+
+        # check if returned auth object is an instance of requests.AuthBase
+        auth = auth_plugin.authenticate()
+        assert isinstance(auth, AuthBase)
+
+        # check if the full signed url and the subscription key are integrated to the request
+        url = "url"
+        req = mock.Mock(headers={}, url=url)
+        auth(req)
+        assert req.url == "this_is_test_full_signed_url"
+        assert req.headers["Ocp-Apim-Subscription-Key"] == "foo"
+
+        # check SAS get request call arguments
+        args, kwargs = mock_requests_get.call_args
+        assert args[0] == auth_plugin.config.auth_uri.format(url=url)
+        auth_plugin_headers = {"Ocp-Apim-Subscription-Key": "foo"}
+        self.assertDictEqual(kwargs["headers"], dict(auth_plugin_headers, **USER_AGENT))
+
+    @mock.patch("eodag.plugins.authentication.sas_auth.requests.get", autospec=True)
+    def test_plugins_auth_sasauth_text_token_authenticate_without_credentials(
+        self, mock_requests_get
+    ):
+        """When a user does not have credentials, SASAuth.authenticate must return
+        a RequestsSASAuth object without his subscription key in its headers
+        """
+        auth_plugin = self.get_auth_plugin("foo_provider")
+
+        auth_plugin.config.credentials = {}
+
+        # mock full signed url get request response
+        mock_requests_get.return_value = mock.Mock()
+        mock_requests_get.return_value.json.return_value = {
+            "msft:expiry": "this_is_test_key_expiration_date",
+            "href": "this_is_test_full_signed_url",
+        }
+
+        # check if returned auth object is an instance of requests.AuthBase
+        auth = auth_plugin.authenticate()
+        assert isinstance(auth, AuthBase)
+
+        # check if only the full signed url is integrated to the request
+        url = "url"
+        req = mock.Mock(headers={}, url=url)
+        auth(req)
+        assert req.url == "this_is_test_full_signed_url"
+
+        # check SAS get request call arguments
+        args, kwargs = mock_requests_get.call_args
+        assert args[0] == auth_plugin.config.auth_uri.format(url=url)
+        # check if headers only has the user agent as a request call argument
+        assert kwargs["headers"] == USER_AGENT
+
+    @mock.patch("eodag.plugins.authentication.sas_auth.requests.get", autospec=True)
+    def test_plugins_auth_sasauth_request_error(self, mock_requests_get):
+        """SASAuth.authenticate must raise an AuthenticationError if an error occurs"""
+        auth_plugin = self.get_auth_plugin("foo_provider")
+
+        auth_plugin.config.credentials = {"apikey": "foo"}
+
+        # mock SAS get request response
+        mock_requests_get.side_effect = RequestException()
+
+        # check if returned auth object is an instance of requests.AuthBase
+        auth = auth_plugin.authenticate()
+        assert isinstance(auth, AuthBase)
+
+        req = mock.Mock(headers={}, url="url")
+        with self.assertRaises(AuthenticationError):
+            auth(req)
