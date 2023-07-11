8de8f849f0fdce1aaf9d17786835fc8aeae4fe1f
Julia Lahovnik <julia.lahovnik@csgroup.eu>
2023-07-11 17:34:04 +0200

    feat: continue work for adding wekeo provider
    format the data in the body of the data request, fetch results of job, transform results to EOProducts

--- eodag/plugins/search/data_request_search.py
+++ eodag/plugins/search/data_request_search.py
@@ -2,9 +2,10 @@ import logging
 
 import requests
 
-from eodag.api.product.metadata_mapping import format_query_params
+from eodag import EOProduct
+from eodag.api.product.metadata_mapping import format_query_params, properties_from_json
 from eodag.plugins.search.base import Search
-from eodag.utils import GENERIC_PRODUCT_TYPE
+from eodag.utils import GENERIC_PRODUCT_TYPE, string_to_jsonpath
 
 logger = logging.getLogger("eodag.plugins.search.data_request_search")
 
@@ -20,7 +21,7 @@ class DataRequestSearch(Search):
     def __init__(self, provider, config):
         super(DataRequestSearch, self).__init__(provider, config)
         self.config.__dict__.setdefault("result_type", "json")
-        self.config.__dict__.setdefault("results_entry", "features")
+        self.config.__dict__.setdefault("results_entry", "content")
         self.config.__dict__.setdefault("pagination", {})
         self.config.__dict__.setdefault("free_text_search_operations", {})
 
@@ -38,18 +39,21 @@ class DataRequestSearch(Search):
         """
         product_type = kwargs.get("productType", None)
         provider_product_type = self._map_product_type(product_type)
+        kwargs["productType"] = provider_product_type
+        print(provider_product_type)
         data_request_id = self._create_data_request(provider_product_type, **kwargs)
         request_finished = False
         while not request_finished:
             request_finished = self._check_request_status(data_request_id)
+        logger.info("search job for product_type %s finished", provider_product_type)
         result = self._get_result_data(data_request_id)
+        logger.info("result retrieved from search job")
         return self._convert_result_data(result)
 
     def _create_data_request(self, product_type, **kwargs):
         headers = getattr(self.auth, "headers", "")
         try:
             metadata_url = self.config.metadata_url + product_type
-            print(self.auth)
             print("h" + str(headers))
             metadata = requests.get(metadata_url, headers=headers)
             print(metadata)
@@ -73,13 +77,14 @@ class DataRequestSearch(Search):
                     "search job for product_type %s could not be created", product_type
                 )
             else:
-                return request_job["jobId"]
+                logger.info("search job for product_type %s created", product_type)
+                return request_job.json()["jobId"]
 
     def _check_request_status(self, data_request_id):
         print(self.config.status_url)
         print(data_request_id)
         status_url = self.config.status_url + data_request_id
-        status_data = requests.get(status_url, headers=self.auth.headers)
+        status_data = requests.get(status_url, headers=self.auth.headers).json()
         if status_data["status"] == "failed":
             logger.error(
                 "data request job has failed, message: %s", status_data["message"]
@@ -87,13 +92,45 @@ class DataRequestSearch(Search):
             raise requests.RequestException
         return status_data["status"] == "completed"
 
-    def _get_result_data(self, param):
-        pass
+    def _get_result_data(self, data_request_id):
+        url = self.config.result_url.format(jobId=data_request_id)
+        try:
+            result = requests.get(url, headers=self.auth.headers).json()
+            print(result)
+            return result
+        except requests.RequestException:
+            logger.error("data from job %s could not be retrieved", data_request_id)
 
-    def _convert_result_data(self, result):
-        pass
+    def _convert_result_data(self, result_data, **kwargs):
+        """Build EOProducts from provider results"""
+        results_entry = self.config.results_entry
+        results = result_data[results_entry]
+        normalize_remaining_count = len(results)
+        logger.debug(
+            "Adapting %s plugin results to eodag product representation"
+            % normalize_remaining_count
+        )
+        products = []
+        for result in results:
+            product = EOProduct(
+                self.provider,
+                properties_from_json(
+                    result,
+                    self.config.metadata_mapping,
+                    discovery_config=getattr(self.config, "discover_metadata", {}),
+                ),
+                **kwargs,
+            )
+            products.append(product)
+        total_items_nb_key_path = string_to_jsonpath(
+            self.config.pagination["total_items_nb_key_path"]
+        )
+        print(total_items_nb_key_path.find(result_data))
+        total_items_nb = total_items_nb_key_path.find(result_data)[0].value
+        print(products)
+        return products, total_items_nb
 
-    def _map_product_type(self, product_type, **kwargs):
+    def _map_product_type(self, product_type):
         """Map the eodag product type to the provider product type"""
         if product_type is None:
             return
