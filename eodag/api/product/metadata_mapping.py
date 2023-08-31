c39d46dbe346121a8c47abd9a7aa439d396bd65c
Augustin Nesson <augustin.nesson@csgroup.eu>
2023-08-31 15:19:36 +0200

    fix: rename convert_get_datetime_dict method to convert_to_datetime_dict

--- eodag/api/product/metadata_mapping.py
+++ eodag/api/product/metadata_mapping.py
@@ -639,7 +639,7 @@ def format_metadata(search_param, *args, **kwargs):
             return product_type
 
         @staticmethod
-        def convert_get_datetime_dict(date: str, format: str) -> dict:
+        def convert_to_datetime_dict(date: str, format: str) -> dict:
             utc_date = MetadataFormatter.convert_to_iso_utc_datetime(date)
             date_object = datetime.strptime(utc_date, "%Y-%m-%dT%H:%M:%S.%fZ")
             if format == "list":
@@ -664,9 +664,9 @@ def format_metadata(search_param, *args, **kwargs):
         @staticmethod
         def convert_get_ecmwf_time(date: str) -> dict:
             return [
-                MetadataFormatter.convert_get_datetime_dict(date, "str")["hour"]
+                MetadataFormatter.convert_to_datetime_dict(date, "str")["hour"]
                 + ":"
-                + MetadataFormatter.convert_get_datetime_dict(date, "str")["minute"]
+                + MetadataFormatter.convert_to_datetime_dict(date, "str")["minute"]
             ]
 
     for match in re.findall(r"\([A-Za-z]+\)", search_param):
--- tests/units/test_metadata_mapping.py
+++ tests/units/test_metadata_mapping.py
@@ -464,8 +464,8 @@ class TestMetadataFormatter(unittest.TestCase):
             "Corine Land Change 2000 2006",
         )
 
-    def test_convert_get_datetime_dict(self):
-        to_format = "{date#get_datetime_dict(list)}"
+    def test_convert_to_datetime_dict(self):
+        to_format = "{date#to_datetime_dict(list)}"
         expected_result = {
             "year": ["2023"],
             "month": ["01"],
@@ -477,7 +477,7 @@ class TestMetadataFormatter(unittest.TestCase):
         self.assertEqual(
             format_metadata(to_format, date="2023-01-31T00:00"), str(expected_result)
         )
-        to_format = "{date#get_datetime_dict(string)}"
+        to_format = "{date#to_datetime_dict(string)}"
         expected_result = {
             "year": "2023",
             "month": "01",
