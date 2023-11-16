82019f642ee925bcf489bf4e5a989a06bbf306e9
Nicola Dalpasso <nicola.dalpasso@csgroup.de>
2023-11-16 09:46:15 +0100

    tests: add test_read_only_home_dir

--- tests/units/test_core.py
+++ tests/units/test_core.py
@@ -1194,14 +1194,15 @@ class TestCoreInvolvingConfDir(unittest.TestCase):
                 except OSError:
                     shutil.rmtree(old_path)
 
-    def execution_involving_conf_dir(self, inspect=None):
+    def execution_involving_conf_dir(self, inspect=None, conf_dir=None):
         """Check that the path(s) inspected (str, list) are created after the instantation
         of EODataAccessGateway. If they were already there, rename them (.old), instantiate,
         check, delete the new files, and restore the existing files to there previous name."""
         if inspect is not None:
+            if conf_dir is None:
+                conf_dir = os.path.join(os.path.expanduser("~"), ".config", "eodag")
             if isinstance(inspect, str):
                 inspect = [inspect]
-            conf_dir = os.path.join(os.path.expanduser("~"), ".config", "eodag")
             olds = []
             currents = []
             for inspected in inspect:
@@ -1232,6 +1233,31 @@ class TestCoreInvolvingConfDir(unittest.TestCase):
     def test_core_object_creates_locations_standard_location(self):
         """The core object must create a locations config file and a shp dir in standard user config location on instantiation"""  # noqa
         self.execution_involving_conf_dir(inspect=["locations.yml", "shp"])
+
+    @mock.patch("eodag.utils.makedirs")
+    def test_read_only_home_dir(self, mock_makedirs):
+        def mock_makedirs_side_effect(dirpath: str):
+            if dirpath.startswith("~") or dirpath.startswith("/home"):
+                raise OSError("Mock makedirs error")
+            else:
+                return unittest.mock.DEFAULT
+
+        # standard directory
+        home_dir = os.path.join(os.path.expanduser("~"), ".config", "eodag")
+        self.execution_involving_conf_dir(inspect="eodag.yml", conf_dir=home_dir)
+
+        # user defined directory
+        user_dir = os.path.join(os.path.expanduser("~"), ".config", "another_eodag")
+        os.environ["EODAG_CFG_DIR"] = user_dir
+        self.execution_involving_conf_dir(inspect="eodag.yml", conf_dir=user_dir)
+        shutil.rmtree(user_dir)
+        del os.environ["EODAG_CFG_DIR"]
+
+        # fallback temporary folder
+        mock_makedirs.side_effect = mock_makedirs_side_effect
+        dag = EODataAccessGateway()
+        self.assertTrue(os.path.exists(os.path.join(dag.conf_dir, "eodag.yml")))
+        shutil.rmtree(dag.conf_dir)
 
 
 class TestCoreGeometry(TestCoreBase):
