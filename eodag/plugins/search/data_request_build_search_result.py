66ead2d88def1128df15edbb7ccbf4db765a02db
Julia Lahovnik <julia.lahovnik@csgroup.eu>
2023-11-24 16:12:37 +0100

    refactor: only create polytope request in search by id

--- eodag/plugins/search/data_request_build_search_result.py
+++ eodag/plugins/search/data_request_build_search_result.py
@@ -1,4 +1,6 @@
 import logging
+import re
+from datetime import datetime
 
 import requests
 from requests import Session
@@ -26,37 +28,45 @@ class DataRequestBuildSearchResult(BuildPostSearchResult):
 
     def do_search(self, *args, **kwargs):
         """Should perform the actual search request."""
-        product_type = kwargs.pop("productType", None)
-        keywords = {k: v for k, v in kwargs.items() if k != "auth" and v is not None}
-        self.product_type_def_params = self.get_product_type_def_params(
-            product_type, **kwargs
-        )
-
-        # Add to the query, the queryable parameters set in the provider product type definition
-        keywords.update(
-            {
-                k: v
-                for k, v in self.product_type_def_params.items()
-                if k not in keywords.keys()
-                and k in self.config.metadata_mapping.keys()
-                and isinstance(self.config.metadata_mapping[k], list)
+        if "id" in kwargs:
+            product_type = kwargs.pop("productType", None)
+            keywords = {
+                k: v for k, v in kwargs.items() if k != "auth" and v is not None
             }
-        )
-        self.request_params = format_query_params(product_type, self.config, **keywords)
-        collection = self._map_product_type(product_type)
-        if not collection:
-            raise MisconfiguredError
-        request_url = self.config.data_request_url.format(collection=collection)
-        self.request_url = request_url
-        request_body = {"verb": "retrieve", "request": self.request_params}
-        request_header = USER_AGENT
-        s = Session()
-        request = requests.Request(
-            method="POST", url=request_url, headers=request_header, json=request_body
-        )
-        prep = request.prepare()
-        self.auth(prep)
-        s.send(prep)
+            self.product_type_def_params = self.get_product_type_def_params(
+                product_type, **kwargs
+            )
+
+            # Add to the query, the queryable parameters set in the provider product type definition
+            keywords.update(
+                {
+                    k: v
+                    for k, v in self.product_type_def_params.items()
+                    if k not in keywords.keys()
+                    and k in self.config.metadata_mapping.keys()
+                    and isinstance(self.config.metadata_mapping[k], list)
+                }
+            )
+            self.request_params = format_query_params(
+                product_type, self.config, **keywords
+            )
+            collection = self._map_product_type(product_type)
+            if not collection:
+                raise MisconfiguredError
+            request_url = self.config.data_request_url.format(collection=collection)
+            self.request_url = request_url
+            request_body = {"verb": "retrieve", "request": self.request_params}
+            request_header = USER_AGENT
+            s = Session()
+            request = requests.Request(
+                method="POST",
+                url=request_url,
+                headers=request_header,
+                json=request_body,
+            )
+            prep = request.prepare()
+            self.auth(prep)
+            s.send(prep)
 
         return [{}]
 
@@ -64,6 +74,18 @@ class DataRequestBuildSearchResult(BuildPostSearchResult):
         self, product_type=None, items_per_page=None, page=None, count=True, **kwargs
     ):
         """query results"""
+        if "id" in kwargs:
+            dates_str = re.search("[0-9]{8}_[0-9]{8}", kwargs["id"]).group()
+            dates = dates_str.split("_")
+            start_date = datetime.strptime(dates[0], "%Y%m%d")
+            kwargs["startTimeFromAscendingNode"] = start_date.strftime(
+                "%Y-%m-%dT%H:%M:%SZ"
+            )
+            end_date = datetime.strptime(dates[1], "%Y%m%d")
+            kwargs["completionTimeFromAscendingNode"] = end_date.strftime(
+                "%Y-%m-%dT%H:%M:%SZ"
+            )
+
         return BuildPostSearchResult.query(
             self, items_per_page=items_per_page, page=page, count=count, **kwargs
         )
@@ -71,31 +93,31 @@ class DataRequestBuildSearchResult(BuildPostSearchResult):
     def normalize_results(self, results, **kwargs):
         """create a formatted result"""
         products = BuildPostSearchResult.normalize_results(self, results, **kwargs)
-        request_header = USER_AGENT
-        request = requests.Request(
-            method="GET", url=self.request_url, headers=request_header
-        )
-        s = Session()
-        prep = request.prepare()
-        self.auth(prep)
-        collection_requests = s.send(prep).json()
-        results_entry = getattr(self.config, "results_entry", None)
-        if results_entry:
-            collection_requests = collection_requests[results_entry]
-        params_entry = getattr(self.config, "request_params_entry")
-        request_id = ""
-        for req in collection_requests:
-            if req[params_entry] == str(self.request_params):
-                request_id = req["id"]
-        if not request_id:
-            raise NotAvailableError(
-                "request with parameters %s not found", str(self.request_params)
+        if "id" in kwargs:
+            request_header = USER_AGENT
+            request = requests.Request(
+                method="GET", url=self.request_url, headers=request_header
             )
-        for p in products:
-            p.properties["downloadLink"] = p.properties["downloadLink"].replace(
-                "request_id", str(request_id)
-            )
-        print(products[0].properties)
+            s = Session()
+            prep = request.prepare()
+            self.auth(prep)
+            collection_requests = s.send(prep).json()
+            results_entry = getattr(self.config, "results_entry", None)
+            if results_entry:
+                collection_requests = collection_requests[results_entry]
+            params_entry = getattr(self.config, "request_params_entry")
+            request_id = ""
+            for req in collection_requests:
+                if req[params_entry] == str(self.request_params):
+                    request_id = req["id"]
+            if not request_id:
+                raise NotAvailableError(
+                    "request with parameters %s not found", str(self.request_params)
+                )
+            for p in products:
+                p.properties["downloadLink"] = p.properties["downloadLink"].replace(
+                    "request_id", str(request_id)
+                )
         return products
 
     def clear(self):
