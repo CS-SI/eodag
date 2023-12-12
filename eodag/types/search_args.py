e59a308488fa8cb689ff4b6f517a91abb391f7d4
LAMBARE Aubin <aubin.lambare@csgroup.eu>
2023-12-12 01:09:17 +0100

    feat: init search model

--- eodag/types/search_args.py
+++ eodag/types/search_args.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from datetime import datetime
-from typing import Dict, List, Optional, Tuple, Union, cast
+from typing import Any, Dict, List, Optional, Tuple, Union, cast
 
 from pydantic import BaseModel, ConfigDict, Field, conint, field_validator
 from shapely import wkt
@@ -60,21 +60,21 @@ class SearchArgs(BaseModel):
 
     @field_validator("geom", mode="before")
     @classmethod
-    def check_geom(cls, v: GeomArgs) -> BaseGeometry:
+    def check_geom(cls, v: Any) -> BaseGeometry:
         """Validate geom"""
         # GeoJSON geometry
-        if isinstance(v, dict) and v.get("type") in GEOMETRY_TYPES:
+        if isinstance(v, dict) and v.get("type") in GEOMETRY_TYPES: # type: ignore
             return cast(BaseGeometry, shape(v))
 
         # Bounding Box
         if isinstance(v, (list, tuple, dict)):
-            return BBox(v).to_polygon()
+            return BBox(v).to_polygon() # type: ignore
 
         if isinstance(v, str):
             # WKT geometry
             try:
-                return cast(Polygon, wkt.loads(v))
-            except GEOSException as e:
+                return cast(Polygon, wkt.loads(v))  # type: ignore
+            except GEOSException as e:  # type: ignore
                 raise ValueError(f"Invalid geometry WKT string: {v}") from e
 
         if isinstance(v, BaseGeometry):
