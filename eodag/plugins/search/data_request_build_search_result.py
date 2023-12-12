cd2ed4b31a9ecf797afafce53b413a50bbd83cba
Julia Lahovnik <julia.lahovnik@csgroup.eu>
2023-12-12 17:52:15 +0100

    fix: add default dates

--- eodag/plugins/search/data_request_build_search_result.py
+++ eodag/plugins/search/data_request_build_search_result.py
@@ -8,10 +8,11 @@ from requests import Session
 from eodag.api.product.metadata_mapping import format_query_params
 from eodag.plugins.search.base import Search
 from eodag.plugins.search.build_search_result import BuildPostSearchResult
+from eodag.rest.stac import DEFAULT_MISSION_START_DATE
 from eodag.utils import USER_AGENT, MisconfiguredError
 from eodag.utils.exceptions import NotAvailableError
 
-logger = logging.getLogger("eodag.apis.polytope")
+logger = logging.getLogger("eodag.search.data_request_build_search_result")
 
 
 class DataRequestBuildSearchResult(BuildPostSearchResult):
@@ -87,6 +88,15 @@ class DataRequestBuildSearchResult(BuildPostSearchResult):
             kwargs["completionTimeFromAscendingNode"] = end_date.strftime(
                 "%Y-%m-%dT%H:%M:%SZ"
             )
+        else:
+            if "startTimeFromAscendingNode" not in kwargs:
+                kwargs["startTimeFromAscendingNode"] = getattr(
+                    self.config, "product_type_config", {}
+                ).get("missionStartDate", DEFAULT_MISSION_START_DATE)
+            if "completionTimeFromAscendingNode" not in kwargs:
+                kwargs["completionTimeFromAscendingNode"] = getattr(
+                    self.config, "product_type_config", {}
+                ).get("missionEndDate", datetime.utcnow().isoformat())
 
         return BuildPostSearchResult.query(
             self, items_per_page=items_per_page, page=page, count=count, **kwargs
