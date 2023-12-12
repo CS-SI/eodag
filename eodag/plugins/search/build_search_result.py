8e913553f27c44b21cc048f43065d215241c6c67
Julia Lahovnik <julia.lahovnik@csgroup.eu>
2023-12-12 17:03:00 +0100

    fix: duplications due to merge conflict

--- eodag/plugins/search/build_search_result.py
+++ eodag/plugins/search/build_search_result.py
@@ -127,33 +127,19 @@ class BuildPostSearchResult(PostJsonSearch):
                 next_page_query_obj = orjson.loads(
                     self.config.pagination["next_page_query_obj"].format()
                 )
+                unpaginated_query_params = {
+                    k: v[0] if (isinstance(v, list) and len(v) == 1) else v
+                    for k, v in self.query_params.items()
+                    if (k, v) not in next_page_query_obj.items()
+                }
             else:
-                # update result with query parameters without pagination (or search-only params)
-                if (
-                    "next_page_query_obj" in self.config.pagination
-                    and isinstance(self.config.pagination["next_page_query_obj"], str)
-                    and hasattr(self, "query_params_unpaginated")
-                ):
-                    unpaginated_query_params = self.query_params_unpaginated
-                elif "next_page_query_obj" in self.config.pagination and isinstance(
-                    self.config.pagination["next_page_query_obj"], str
-                ):
-                    next_page_query_obj = orjson.loads(
-                        self.config.pagination["next_page_query_obj"].format()
-                    )
-                    unpaginated_query_params = {
-                        k: v[0] if (isinstance(v, list) and len(v) == 1) else v
-                        for k, v in self.query_params.items()
-                        if (k, v) not in next_page_query_obj.items()
-                    }
-                else:
-                    unpaginated_query_params = self.query_params
+                unpaginated_query_params = self.query_params
 
                 # query hash, will be used to build a product id
-                sorted_unpaginated_query_params = dict_items_recursive_sort(
-                    unpaginated_query_params
-                )
-                qs = geojson.dumps(sorted_unpaginated_query_params)
+            sorted_unpaginated_query_params = dict_items_recursive_sort(
+                unpaginated_query_params
+            )
+            qs = geojson.dumps(sorted_unpaginated_query_params)
 
         query_hash = hashlib.sha1(str(qs).encode("UTF-8")).hexdigest()
 
