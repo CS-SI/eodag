24b0965ac79d7215f67f5be63fde5cea18b8e118
Sylvain Brunato <sylvain.brunato@c-s.fr>
2024-01-16 17:24:16 +0100

    refactor: error handling and copyright

--- eodag/plugins/search/creodias_s3.py
+++ eodag/plugins/search/creodias_s3.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024, CS GROUP - France, https://www.csgroup.eu/
+#
+# This file is part of EODAG project
+#     https://www.github.com/CS-SI/EODAG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import logging
 from types import MethodType
 from typing import Any, Dict, List
@@ -10,7 +27,7 @@ from eodag import EOProduct
 from eodag.config import PluginConfig
 from eodag.plugins.authentication.aws_auth import AwsAuth
 from eodag.plugins.search.qssearch import QueryStringSearch
-from eodag.utils.exceptions import RequestError
+from eodag.utils.exceptions import AuthenticationError, RequestError
 
 DATA_EXTENSIONS = ["jp2", "tiff", "nc", "grib"]
 logger = logging.getLogger("eodag.search.creodiass3")
@@ -34,8 +51,7 @@ def patched_register_downloader(self, downloader, authenticator):
     try:
         _update_assets(self, downloader.config, authenticator)
     except BotoCoreError as e:
-        logger.error(f"could not update assets: {str(e)}")
-        raise RequestError(e)
+        raise RequestError(f"could not update assets: {str(e)}") from e
 
 
 def _update_assets(product: EOProduct, config: PluginConfig, auth: AwsAuth):
@@ -77,8 +93,15 @@ def _update_assets(product: EOProduct, config: PluginConfig, auth: AwsAuth):
 
             # update driver
             product.driver = product.get_driver()
-        except botocore.exceptions.ClientError:
-            logger.error("assets for product %s could not be found", prefix)
+        except botocore.exceptions.ClientError as e:
+            if str(auth.config.auth_error_code) in str(e):
+                raise AuthenticationError(
+                    f"Authentication failed on {config.base_uri} s3"
+                ) from e
+            else:
+                raise RequestError(
+                    "assets for product %s could not be found", prefix
+                ) from e
 
 
 class CreodiasS3Search(QueryStringSearch):
