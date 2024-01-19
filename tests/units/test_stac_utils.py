068c7770c0eee2adff69e13cc21e7509ccbba5d9
LAMBARE Aubin <aubin.lambare@csgroup.eu>
2024-01-19 21:09:39 +0100

    feat: raise errors in search

--- tests/units/test_stac_utils.py
+++ tests/units/test_stac_utils.py
@@ -22,19 +22,15 @@ import os
 import unittest
 from datetime import datetime, timezone
 from tempfile import TemporaryDirectory
-from urllib.parse import quote_plus, unquote_plus
 
-import orjson
-import pytest
 from pygeofilter import ast
 from pygeofilter.values import Geometry
 
 import eodag.rest.utils.rfc3339 as rfc3339
 from eodag.rest.types.stac_search import SearchPostRequest
 from eodag.rest.utils.cql_evaluate import EodagEvaluator
-from eodag.utils.exceptions import ValidationError
 from tests import TEST_RESOURCES_PATH, mock
-from tests.context import RequestError, SearchResult
+from tests.context import SearchResult
 from tests.utils import mock_request
 
 
@@ -115,258 +111,6 @@ class TestStacUtils(unittest.TestCase):
         )
         self.assertEqual(json_dict, {"collections": ["S1_SAR_GRD"]})
 
-    def test_get_stac_catalogs(self):
-        """get_stac_catalogs runs without any error"""
-        self.rest_utils.get_stac_catalogs(url="")
-
-    def test_get_stac_collection_by_id(self):
-        """get_stac_collection_by_id runs without any error"""
-        r = self.rest_utils.get_stac_collection_by_id(
-            url="", root="", collection_id="S2_MSI_L1C"
-        )
-        self.assertIsNotNone(r)
-        self.assertEqual(8, len(r["providers"]))
-        self.assertEqual(1, r["providers"][0]["priority"])
-        self.assertEqual("peps", r["providers"][0]["name"])
-        self.assertEqual(["host"], r["providers"][0]["roles"])
-        self.assertEqual("https://peps.cnes.fr", r["providers"][0]["url"])
-        self.assertTrue(
-            r["providers"][0]["description"].startswith(
-                'The PEPS platform, the French "mirror site"'
-            )
-        )
-
-    def test_get_stac_collections(self):
-        """get_stac_collections runs without any error"""
-        self.rest_utils.get_stac_collections(url="", root="", arguments={})
-
-    def test_get_stac_conformance(self):
-        """get_stac_conformance runs without any error"""
-        self.rest_utils.get_stac_conformance()
-
-    def test_get_stac_extension_oseo(self):
-        """get_stac_extension_oseo runs without any error"""
-        self.rest_utils.get_stac_extension_oseo(url="")
-
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch.do_search", autospec=True
-    )
-    def test_get_stac_item_by_id(self, mock_do_search):
-        """get_stac_item_by_id returns None if no StacItem was found"""
-        mock_do_search.return_value = [
-            {
-                "geometry": "POINT (0 0)",
-                "properties": {"productIdentifier": "foo", "title": "foo"},
-            }
-        ]
-        self.assertIsNotNone(
-            self.rest_utils.get_stac_item_by_id(
-                url="", item_id="foo", catalogs=["S2_MSI_L1C"]
-            )
-        )
-        mock_do_search.return_value = []
-        self.assertIsNone(
-            self.rest_utils.get_stac_item_by_id(
-                url="", item_id="", catalogs=["S3_MSI_L1C"]
-            )
-        )
-
-    def test_get_templates_path(self):
-        """get_templates_path returns an existing dir path"""
-        with pytest.warns(
-            DeprecationWarning,
-            match="Call to deprecated function/method get_templates_path",
-        ):
-            self.assertTrue(os.path.isdir(self.rest_utils.get_templates_path()))
-
-    def test_search_bbox(self):
-        pass  # TODO
-
-    def test_search_product_by_id(self):
-        pass  # TODO
-
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch.do_search",
-        autospec=True,
-        return_value=[
-            {
-                "geometry": "POINT (0 0)",
-                "properties": {"productIdentifier": "foo", "title": "foo"},
-            }
-        ],
-    )
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch.count_hits",
-        autospec=True,
-        return_value=1,
-    )
-    def test_search_products(self, mock_count_hits, mock_do_search):
-        """search_products runs without any error"""
-        self.rest_utils.search_products("S2_MSI_L1C", {})
-        self.rest_utils.search_products("S2_MSI_L1C", {"unserialized": "true"})
-
-        # STAC formatted
-        self.rest_utils.search_products(
-            "S2_MSI_L1C",
-            {
-                "intersects": {
-                    "type": "Polygon",
-                    "coordinates": [
-                        [
-                            [0.25, 43.2],
-                            [0.25, 43.9],
-                            [2.8, 43.9],
-                            [2.8, 43.2],
-                            [0.25, 43.2],
-                        ]
-                    ],
-                },
-                "query": {"eo:cloud_cover": {"lte": 50}},
-                "dtstart": "2020-02-01T00:00:00.000Z",
-                "dtend": "2021-02-20T00:00:00.000Z",
-                "product_type": "S2_MSI_L1C",
-                "unserialized": "true",
-            },
-        )
-        call_args, call_kwargs = mock_do_search.call_args
-        # check if call_kwargs contains subset
-        self.assertLessEqual(
-            {
-                "productType": "S2_MSI_L1C",
-                "cloudCover": 50,
-                "startTimeFromAscendingNode": "2020-02-01T00:00:00",
-                "completionTimeFromAscendingNode": "2021-02-20T00:00:00",
-            }.items(),
-            call_kwargs.items(),
-        )
-        self.assertEqual(call_kwargs["geometry"].bounds, (0.25, 43.2, 2.8, 43.9))
-
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch.do_search",
-        autospec=True,
-        side_effect=RequestError,
-    )
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch.count_hits",
-        autospec=True,
-        side_effect=RequestError,
-    )
-    def test_search_products_fail(self, mock_count_hits, mock_do_search):
-        """search_products fail must return an error"""
-        with self.assertRaisesRegex(
-            RequestError,
-            r"No result could be obtained from any available provider",
-        ):
-            self.rest_utils.search_products("S2_MSI_L1C", {})
-
-    @mock.patch(
-        "eodag.plugins.search.qssearch.PostJsonSearch._request",
-        autospec=True,
-    )
-    def test_search_stac_items_with_stac_providers(self, mock__request):
-        """search_stac_items runs without any error with stac providers"""
-        # mock the PostJsonSearch request with the S2_MSI_L1C earth_search response search dictionary
-        mock__request.return_value = mock.Mock()
-        mock__request.return_value.json.return_value = (
-            self.earth_search_resp_search_json
-        )
-        self.rest_utils.eodag_api.set_preferred_provider("peps")
-
-        response = self.rest_utils.search_stac_items(
-            url="http://foo/search",
-            arguments={"collections": "S2_MSI_L1C"},
-            root="http://foo",
-            catalogs=[],
-            provider="earth_search",
-        )
-
-        mock__request.assert_called()
-
-        # check that default assets have been added to the response
-        self.assertTrue(
-            "downloadLink", "thumbnail" in response["features"][0]["assets"].keys()
-        )
-        # check that assets from the provider response search are reformatted in the response
-        product_id = self.earth_search_resp_search_json["features"][0]["properties"][
-            "s2:product_uri"
-        ].replace(".SAFE", "")
-        for (k, v) in self.earth_search_resp_search_json["features"][0][
-            "assets"
-        ].items():
-            self.assertIn(k, response["features"][0]["assets"].keys())
-            # check asset server-mode download link
-            self.assertEqual(
-                response["features"][0]["assets"][k]["href"],
-                f"http://foo/collections/S2_MSI_L1C/items/{product_id}/download/{k}?provider=earth_search",
-            )
-            # check asset origin download link
-            self.assertEqual(
-                response["features"][0]["assets"][k]["alternate"]["origin"]["href"],
-                self.earth_search_resp_search_json["features"][0]["assets"][k]["href"],
-            )
-        # preferred provider should not be changed
-        self.assertEqual("peps", self.rest_utils.eodag_api.get_preferred_provider()[0])
-
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch._request",
-        autospec=True,
-    )
-    def test_search_stac_items_with_non_stac_providers(self, mock__request):
-        """search_stac_items runs without any error with non-stac providers"""
-        # mock the QueryStringSearch request with the S2_MSI_L1C peps response search dictionary
-        mock__request.return_value = mock.Mock()
-        mock__request.return_value.json.return_value = self.peps_resp_search_json
-
-        response = self.rest_utils.search_stac_items(
-            url="http://foo/search",
-            arguments={},
-            root="http://foo/",
-            catalogs=["S2_MSI_L1C"],
-            provider="peps",
-        )
-
-        mock__request.assert_called()
-
-        # check that default assets have been added to the response
-        self.assertTrue(
-            "downloadLink", "thumbnail" in response["features"][0]["assets"].keys()
-        )
-        # check that no other asset have also been added to the response
-        self.assertEqual(len(response["features"][0]["assets"]), 2)
-
-    @mock.patch(
-        "eodag.plugins.search.qssearch.QueryStringSearch._request",
-        autospec=True,
-    )
-    def test_search_stac_items_get(self, mock__request):
-        """search_stac_items runs with GET method"""
-        # mock the QueryStringSearch request with the S2_MSI_L1C peps response search dictionary
-        mock__request.return_value = mock.Mock()
-        mock__request.return_value.json.return_value = self.peps_resp_search_json
-
-        response = self.rest_utils.search_stac_items(
-            url="http://foo/search",
-            arguments={"collections": "S2_MSI_L1C"},
-            root="http://foo/",
-            method="GET",
-        )
-
-        mock__request.assert_called()
-
-        next_link = [link for link in response["links"] if link["rel"] == "next"][0]
-
-        json_obj = {"key": "value with spaces"}
-        json_str_quoted = quote_plus(orjson.dumps(json_obj).decode())
-        json_str_unquoted = unquote_plus(json_str_quoted)
-        self.assertEqual(
-            self.rest_utils.str2json("key", json_str_quoted),
-            orjson.loads(json_str_unquoted),
-        )
-
-        with pytest.raises(ValidationError) as exc_info:
-            self.rest_utils.str2json("key", "invalid json")
-            self.assertEqual("key: Incorrect JSON object", str(exc_info.value))
-
     def test_str2list(self):
         """str2list convert a str variable to a list variable"""
         self.assertIsNone(self.rest_utils.str2list(None))
@@ -397,6 +141,40 @@ class TestStacUtils(unittest.TestCase):
         )
         self.assertFalse(self.rest_utils.is_dict_str_any("not a dict"))
         self.assertFalse(self.rest_utils.is_dict_str_any(None))
+
+    def test_flatten_list(self):
+        """flatten_list convert list of list to single list"""
+        self.assertEqual(
+            self.rest_utils.flatten_list([1, 2, [3, 4, [5, 6], 7], 8]),
+            [1, 2, 3, 4, 5, 6, 7, 8],
+        )
+        self.assertEqual(
+            self.rest_utils.flatten_list(["a", ["b", ["c", "d"], "e"], "f"]),
+            ["a", "b", "c", "d", "e", "f"],
+        )
+        self.assertEqual(self.rest_utils.flatten_list([1, 2, 3]), [1, 2, 3])
+        self.assertEqual(self.rest_utils.flatten_list([]), [])
+        self.assertEqual(
+            self.rest_utils.flatten_list([1, [2, [3, [4, [5]]]]]), [1, 2, 3, 4, 5]
+        )
+
+    def test_list_to_str_list(self):
+        """
+        list_to_str_list convert a List[Any] to a List[str].
+        It raises a TypeError if the convertion cannot be done
+        """
+        self.assertEqual(self.rest_utils.list_to_str_list([1, 2, 3]), ["1", "2", "3"])
+        self.assertEqual(
+            self.rest_utils.list_to_str_list(["a", "b", "c"]), ["a", "b", "c"]
+        )
+        self.assertEqual(
+            self.rest_utils.list_to_str_list([1.1, 2.2, 3.3]), ["1.1", "2.2", "3.3"]
+        )
+        self.assertEqual(self.rest_utils.list_to_str_list([]), [])
+        self.assertEqual(
+            self.rest_utils.list_to_str_list([1, "2", 3.3, [4, 5]]),
+            ["1", "2", "3.3", "[4, 5]"],
+        )
 
     def test_get_next_link_post(self):
         """Verify search next link for POST request"""
