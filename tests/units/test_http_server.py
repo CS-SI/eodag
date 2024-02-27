88b4df734c6cca66f83f2a41126ba75fd657611b
Diwakar Kushwaha <diwakar.kushwaha@csgroup.de>
2024-02-27 10:53:17 +0100

    Fixed the failing test cases

--- tests/units/test_http_server.py
+++ tests/units/test_http_server.py
@@ -1137,6 +1137,7 @@ class RequestTestCase(unittest.TestCase):
             url="https://planetarycomputer.microsoft.com/api/stac/v1/search/../queryables",
             timeout=HTTP_REQ_TIMEOUT,
             headers=USER_AGENT,
+            verify=True,
         )
         # TODO: with an unsupported provider
 
@@ -1179,6 +1180,7 @@ class RequestTestCase(unittest.TestCase):
             "sentinel-1-grd/queryables",
             timeout=HTTP_REQ_TIMEOUT,
             headers=USER_AGENT,
+            verify=True,
         )
         # returned queryables
         self.assertListEqual(
@@ -1212,6 +1214,7 @@ class RequestTestCase(unittest.TestCase):
             "sentinel-1-grd/queryables",
             timeout=HTTP_REQ_TIMEOUT,
             headers=USER_AGENT,
+            verify=True,
         )
 
         self.assertListEqual(
