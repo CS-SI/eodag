ef3bf78038a5cd81e6e8e70a809f24e04c6b3ab7
Nicola Dalpasso <nicola.dalpasso@csgroup.de>
2024-03-01 08:43:18 +0100

    fix: remove unused import after rebase

--- tests/units/test_apis_plugins.py
+++ tests/units/test_apis_plugins.py
@@ -16,9 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import ast
-import io
 import json
-import logging
 import os
 import unittest
 from datetime import datetime
