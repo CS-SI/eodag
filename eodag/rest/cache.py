0b7828eda730cb69b2a9b01c4d7d2138781502a7
LAMBARE Aubin <aubin.lambare@csgroup.eu>
2024-03-26 08:27:32 +0100

    WIP download cache

--- eodag/rest/cache.py
+++ eodag/rest/cache.py
@@ -16,7 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-from typing import Any, Callable, Coroutine, Dict, TypeVar
+import time
+from typing import Any, Callable, Coroutine, Dict, Optional, TypeVar
 
 import orjson
 from cachetools import LRUCache
@@ -24,6 +25,7 @@ from fastapi import FastAPI, Request
 from redis.asyncio import Redis
 
 from eodag.rest.config import Settings
+from eodag.utils import deepcopy
 
 logger = logging.getLogger("eodag.rest.utils")
 
@@ -34,6 +36,8 @@ def init_cache(app: FastAPI) -> None:
     """Connect to redis or default to local cache"""
     settings = Settings.from_environment()
 
+    app.state.cache = LRUCache(maxsize=settings.local_cache_maxsize)
+
     if settings.redis_hostname:
         r = Redis(
             host=settings.redis_hostname,
@@ -43,42 +47,6 @@ def init_cache(app: FastAPI) -> None:
             decode_responses=True,
         )
         app.state.redis = r
-    else:
-        app.state.cache = LRUCache(maxsize=settings.local_cache_maxsize)
-
-
-async def redis_cached(
-    fn: Callable[[], Coroutine[Any, Any, T]], cache_key: str, request: Request
-) -> T:
-    """Either get the result from Redis or run the function and cache the result."""
-    settings = Settings.from_environment()
-
-    host = request.url.hostname
-    host_cache_key = f"{cache_key}:{host}"
-
-    try:
-        r: Redis[Any] = request.app.state.redis
-        cached: Any = await r.get(host_cache_key)
-        if cached:
-            logger.debug("Cache result hit")
-            return orjson.loads(cached)  # type: ignore
-    except Exception as e:
-        logger.error(f"Error in cache: {e}")
-        if settings.debug:
-            raise
-
-    result = await fn()
-
-    logger.debug("Perf: cacheable resource fetch time")
-    try:
-        if r:
-            await r.set(host_cache_key, orjson.dumps(result), settings.redis_ttl)  # type: ignore
-    except Exception as e:
-        logger.error(f"Error in cache: {e}")
-        if settings.debug:
-            raise
-
-    return result
 
 
 async def local_cached(
@@ -108,3 +76,97 @@ async def local_cached(
             raise
 
     return result
+
+
+async def cached_item_collection(
+    fn: Callable[[], Coroutine[Any, Any, Dict[str, Any]]],
+    cache_key: str,
+    request: Request,
+) -> Dict[str, Any]:
+    """
+    Cache an item collection search result
+    Either get the result from redis or run the function and cache the result.
+    """
+    settings = Settings.from_environment()
+
+    host = request.url.hostname
+    host_cache_key = f"{cache_key}:{host}"
+
+    try:
+        ts = time.perf_counter()
+        r: Redis[Any] = request.app.state.redis
+        cached: Any = await r.get(host_cache_key)
+        if cached:
+            logger.debug("Cache result hit")
+            item_collection: Dict[str, Any] = orjson.loads(cached)
+            for i, item in enumerate(item_collection["features"]):
+                cached_item = await r.get(item)
+                if not cached_item:
+                    raise ValueError(f"item {item} not found")
+                item_collection["features"][i] = orjson.loads(cached_item)
+            te = time.perf_counter()
+            logger.debug(f"Perf: retrieved from cache: {te - ts:0.4f}")
+            return item_collection
+    except Exception as e:
+        logger.error(f"Error in cache: {e}")
+        if settings.debug:
+            raise
+
+    ts = time.perf_counter()
+    item_collection = await fn()
+    te = time.perf_counter()
+
+    logger.debug(f"Perf: cacheable resource fetch time: {te - ts:0.4f}")
+
+    ts = time.perf_counter()
+    cached_item_collection = deepcopy(item_collection)
+    features = cached_item_collection["features"]
+
+    try:
+        if r:
+            # we assume all items belong to the same provider and collection
+            provider = next(
+                p["name"]
+                for p in features[0]["properties"]["providers"]
+                if "host" in p["roles"]
+            )
+            collection = features[0]["collection"]
+            for i, item in enumerate(features):
+                i_key = f'{provider}:{collection}:{item["id"]}:{host}'
+                await r.set(i_key, orjson.dumps(item), settings.redis_ttl)
+                features[i] = i_key
+            await r.set(
+                host_cache_key, orjson.dumps(cached_item_collection), settings.redis_ttl
+            )
+            te = time.perf_counter()
+            logger.debug(f"Perf: cache set: {te - ts:0.4f}")
+    except Exception as e:
+        logger.error(f"Error in cache: {e}")
+        if settings.debug:
+            raise
+
+    return item_collection
+
+
+async def cached_item(
+    request: Request, provider: str, collection: str, item: str
+) -> Optional[Dict[str, Any]]:
+    """
+    Extract cached item from redis
+    """
+    settings = Settings.from_environment()
+
+    host = request.url.hostname
+    host_cache_key = f"{provider}:{collection}:{item}:{host}"
+
+    try:
+        r: Redis[Any] = request.app.state.redis
+        cached: Any = await r.get(host_cache_key)
+        if cached:
+            logger.debug("Cache result hit")
+            return orjson.loads(cached)
+    except Exception as e:
+        logger.error(f"Error in cache: {e}")
+        if settings.debug:
+            raise
+    return None
