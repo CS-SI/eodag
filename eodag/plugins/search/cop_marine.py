336d786e1b1ef6fb9724ad3e4f2e6fe1c2f2c6fb
Julia Lahovnik <julia.lahovnik@csgroup.eu>
2024-05-16 16:48:43 +0200

    refactor: search with cop marine provider without usage of library

--- eodag/plugins/search/cop_marine.py
+++ eodag/plugins/search/cop_marine.py
@@ -1,18 +1,22 @@
 import copy
-import hashlib
 import re
-from typing import Any, List, Optional, Tuple
-from urllib.parse import quote_plus, unquote_plus
+from datetime import datetime
+from typing import Any, Dict, List, Optional, Tuple, cast
+from urllib.parse import urlsplit
 
-import copernicusmarine
-import geojson
+import boto3
+import botocore
+import requests
 from dateutil.parser import isoparse
+from dateutil.utils import today
+from pytz import timezone
 
 from eodag import EOProduct
-from eodag.api.product.metadata_mapping import format_query_params
+from eodag.api.product import AssetsDict
 from eodag.config import PluginConfig
 from eodag.plugins.search.static_stac_search import StaticStacSearch
 from eodag.utils import DEFAULT_ITEMS_PER_PAGE, DEFAULT_PAGE
+from eodag.utils.stac_reader import fetch_stac_collections
 
 
 class CopMarineSearch(StaticStacSearch):
@@ -23,6 +27,33 @@ class CopMarineSearch(StaticStacSearch):
         super().__init__(provider, config)
         # reset to original metadata mapping from config (changed in super class init)
         self.config.metadata_mapping = original_metadata_mapping
+
+    def _get_product_type_info(self, product_type: str) -> Dict[str, Any]:
+        fetch_url = cast(
+            str,
+            self.config.discover_product_types["fetch_url"].format(
+                **self.config.__dict__
+            ),
+        )
+        collections = fetch_stac_collections(
+            fetch_url,
+            max_connections=self.config.max_connections,
+            timeout=int(self.config.timeout),
+            ssl_verify=self.config.ssl_verify,
+        )
+        if "item_discovery" in self.config.discover_product_types:
+            for collection in collections:
+                for link in collection["links"]:
+                    if product_type in link["title"]:
+                        collection_url = (
+                            fetch_url.replace("catalog.stac.json", collection["id"])
+                            + "/"
+                            + product_type
+                            + "/dataset.stac.json"
+                        )
+                        collection_data = requests.get(collection_url).json()
+                        return collection_data
+        return {}
 
     def query(
         self,
@@ -46,72 +77,74 @@ class CopMarineSearch(StaticStacSearch):
         :returns: list of products and total number of products
         :rtype: Tuple[List[EOProduct], Optional[int]]
         """
-        auth = kwargs.pop("auth").authenticate()
-        copernicusmarine.login(
-            username=auth.username, password=auth.password, skip_if_user_logged_in=True
-        )
         product_type = kwargs.get("productType", product_type)
-        bbox = None
-        if "_dc_qs" in kwargs:
-            query_str = unquote_plus(unquote_plus(kwargs["_dc_qs"]))
-            query_params = geojson.loads(query_str)
+        collection_data = self._get_product_type_info(product_type)
+        s3_url = collection_data["assets"]["native"]["href"]
+        url_parts = urlsplit(s3_url)
+        path = url_parts.path
+        endpoint_url = url_parts.scheme + "://" + url_parts.hostname
+        bucket = path.split("/")[1]
+        collection_path = "/".join(path.split("/")[2:])
+        s3_session = boto3.Session()
+        s3_client = s3_session.client(
+            "s3",
+            config=botocore.config.Config(
+                # Configures to use subdomain/virtual calling format.
+                s3={"addressing_style": "virtual"},
+                signature_version=botocore.UNSIGNED,
+            ),
+            endpoint_url=endpoint_url,
+        )
+        if "startTimeFromAscendingNode" in kwargs:
+            start_date = isoparse(kwargs["startTimeFromAscendingNode"])
         else:
-            query_params = format_query_params(product_type, self.config, **kwargs)
-            query_params["dataset_id"] = product_type
-            version = re.search(r"_\d{6}", product_type)
-            if version:
-                query_params["dataset_version"] = version.group().replace("_", "")
-                query_params["dataset_id"] = query_params["dataset_id"].replace(
-                    version.group(), ""
-                )
-            if "bbox" in query_params:
-                bbox = query_params.pop("bbox")
-                query_params["minimum_longitude"] = bbox[0]
-                query_params["minimum_latitude"] = bbox[1]
-                query_params["maximum_longitude"] = bbox[2]
-                query_params["maximum_latitude"] = bbox[3]
+            start_date = isoparse(collection_data["properties"]["datetime"])
+        if "completionTimeFromAscendingNode" in kwargs:
+            end_date = isoparse(kwargs["completionTimeFromAscendingNode"])
+        else:
+            end_date = today()
 
-        ds = copernicusmarine.open_dataset(**query_params)
         products = []
-        if not bbox:
-            min_lon = float(ds.coords["longitude"][0])
-            max_lon = float(ds.coords["longitude"][-1])
-            min_lat = float(ds.coords["latitude"][0])
-            max_lat = float(ds.coords["latitude"][-1])
-            bbox = [min_lon, min_lat, max_lon, max_lat]
-        bbox = [str(value) for value in bbox]
-        for t in ds.coords["time"]:
-            for v in ds.variables:
-                if v not in ds.coords:
-                    new_query_params = copy.deepcopy(query_params)
-                    record_time = isoparse(str(t.values))
-                    new_query_params["start_datetime"] = record_time.isoformat()
-                    new_query_params["end_datetime"] = record_time.isoformat()
-                    new_query_params["variables"] = [v]
-                    qs = geojson.dumps(new_query_params)
-                    query_hash = hashlib.sha1(str(qs).encode("UTF-8")).hexdigest()
-                    product_id = "%s_%s_%s_%s" % (
-                        product_type,
-                        str(t.values).split("T")[0].replace("-", ""),
-                        v,
-                        query_hash,
-                    )
-
-                    properties = {
-                        "id": product_id,
-                        "title": product_id,
-                        "startTimeFromAscendingNode": str(t.values),
-                        "completionTimeFromAscendingNode": str(t.values),
-                        "variable": v,
-                        "_dc_qs": quote_plus(qs),
-                        "geometry": " ".join(bbox),
+        s3_objects = s3_client.list_objects(Bucket=bucket, Prefix=collection_path)
+        if "Contents" not in s3_objects:
+            return [], 0
+        for obj in s3_objects["Contents"]:
+            item_key = obj["Key"]
+            item_dates = re.findall(r"\d{8}", item_key)
+            start_year = item_dates[0][:4]
+            start_month = item_dates[0][4:6]
+            start_day = item_dates[0][6:]
+            item_start = datetime(
+                int(start_year),
+                int(start_month),
+                int(start_day),
+                tzinfo=timezone("UTC"),
+            )
+            if not item_dates or (start_date <= item_start <= end_date):
+                if len(item_dates) > 1:
+                    end_year = item_dates[1][:4]
+                    end_month = item_dates[1][4:6]
+                    end_day = item_dates[1][6:]
+                    item_end = datetime(int(end_year), int(end_month), int(end_day))
+                else:
+                    item_end = item_start
+                id = item_key.split("/")[-1].split(".")[0]
+                properties = {
+                    "start_datetime": item_start.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                    "end_datetime": item_end.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                    "id": id,
+                    "title": id,
+                    "geometry": self.config.metadata_mapping["defaultGeometry"],
+                }
+                assets = {
+                    "native": {
+                        "title": "native",
+                        "href": endpoint_url + "/" + bucket + "/" + item_key,
+                        "type": "application/x-netcdf",
                     }
-                    product = EOProduct(
-                        provider=self.provider,
-                        productType=product_type,
-                        properties=properties,
-                    )
-                    product.remote_location = quote_plus(qs)
-                    product.location = product.remote_location
-                    products.append(product)
+                }
+                product = EOProduct(self.provider, properties, productType=product_type)
+                product.assets = AssetsDict(product, assets)
+                products.append(product)
+
         return products, len(products)
