8954db762012a687c638c859e0dbfe6abbc07011
jlahovnik <julia.lahovnik@csgroup.eu>
2024-12-02 18:06:21 +0100

    fix: wrong import

--- tests/units/test_search_plugins.py
+++ tests/units/test_search_plugins.py
@@ -37,7 +37,7 @@ from pydantic_core import PydanticUndefined
 from requests import RequestException
 from typing_extensions import get_args
 
-from build.lib.eodag.api.product import AssetsDict
+from eodag.api.product import AssetsDict
 from eodag.api.product.metadata_mapping import get_queryable_from_provider
 from eodag.utils import deepcopy
 from eodag.utils.exceptions import UnsupportedProductType
