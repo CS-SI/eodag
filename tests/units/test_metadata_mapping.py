9cbb7f127b8c9ebd965e54aa2d8979b126a72650
jlahovnik <julia.lahovnik@csgroup.eu>
2025-01-31 14:41:29 +0100

    test: convert_to_bounds metadata mapping

--- tests/units/test_metadata_mapping.py
+++ tests/units/test_metadata_mapping.py
@@ -119,6 +119,19 @@ class TestMetadataFormatter(unittest.TestCase):
             "[[2.23, 43.42, 3.68, 43.76], [1.23, 43.42, 1.68, 43.76]]",
         )
 
+    def test_convert_to_bounds(self):
+        to_format = "{fieldname#to_bounds}"
+        geom = get_geometry_from_various(
+            geometry="""MULTIPOLYGON (
+                ((1.23 43.42, 1.23 43.76, 1.68 43.76, 1.68 43.42, 1.23 43.42)),
+                ((2.23 43.42, 2.23 43.76, 3.68 43.76, 3.68 43.42, 2.23 43.42))
+            )"""
+        )
+        self.assertEqual(
+            format_metadata(to_format, fieldname=geom),
+            "[1.23, 43.42, 3.68, 43.76]",
+        )
+
     def test_convert_to_geojson(self):
         to_format = "{fieldname#to_geojson}"
         geom = get_geometry_from_various(geometry="POINT (0.11 1.22)")
