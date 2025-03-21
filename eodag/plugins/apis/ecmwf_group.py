c0555e2716e8c1faf21c683ea8bc1bebeee12496
jlahovnik <julia.lahovnik@csgroup.eu>
2025-03-21 17:48:32 +0100

    fix: server mode download for dedt_lumi

--- eodag/plugins/apis/ecmwf_group.py
+++ eodag/plugins/apis/ecmwf_group.py
@@ -169,3 +169,59 @@ class DedtEcmwfGroupApi(EcmwfGroupApi, OIDCAuthorizationCodeFlowAuth):
     """DEDT ECMWF Group API plugin.
     Override to use the proper authentication method.
     """
+
+    def _check_id(self, product_id: str) -> list[dict[str, Any]]:
+        """Check if the id is the one of an existing job.
+        If the job exists, poll it, otherwise, raise an error.
+
+        :param product: The product to check the id for
+        :param id: The id to check
+        :raises: :class:`~eodag.utils.exceptions.ValidationError`
+        """
+        # set fake properties to make EOProduct initialization possible
+        # among these properties, "title" is set to deal with error while polling
+        product_base = {
+            "id": product_id,
+            "title": product_id,
+            "geometry": DEFAULT_GEOMETRY,
+        }
+
+        product = EOProduct(self.provider, product_base)
+        product.downloader = self
+
+        # update "orderStatusLink" and potential "search_link" properties to match the id from the search request
+        order_status_link = product.downloader.config.order_on_response[
+            "metadata_mapping"
+        ]["orderStatusLink"]
+        search_link = product.downloader.config.order_on_response[
+            "metadata_mapping"
+        ].get("searchLink", "")
+        if not isinstance(order_status_link, str) or not isinstance(search_link, str):
+            return [{}]
+
+        base_order_status_link = "/".join(order_status_link.split("/")[:-1])
+        product.properties["orderStatusLink"] = f"{base_order_status_link}/{product_id}"
+        formatted_search_link = search_link.format(orderId=product_id)
+        search_link_dict = (
+            {"searchLink": formatted_search_link} if formatted_search_link else {}
+        )
+        product.properties.update(search_link_dict)
+
+        auth = product.downloader.authenticate() if product.downloader else None
+
+        # try to poll the job corresponding to the given id
+        try:
+            product.downloader._order_status(product=product, auth=auth)  # type: ignore
+        # when a NotAvailableError is catched, it means the product is not ready and still needs to be polled
+        except NotAvailableError:
+            product.properties["storageStatus"] = STAGING_STATUS
+        except Exception as e:
+            if (
+                isinstance(e, DownloadError) or isinstance(e, ValidationError)
+            ) and "order status could not be checked" in e.args[0]:
+                raise ValidationError(
+                    f"Item {product_id} does not exist with {self.provider}."
+                ) from e
+            raise ValidationError(e.args[0]) from e
+
+        return [product.properties]
