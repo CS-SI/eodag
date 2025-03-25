72bd1a3b8df2e568cde3bf688e193db77e8b938c
Sylvain Brunato <sylvain.brunato@c-s.fr>
2025-03-25 17:08:23 +0100

    refactor: matching_conf usage back in get_auth

--- eodag/plugins/manager.py
+++ eodag/plugins/manager.py
@@ -32,7 +32,6 @@ from eodag.config import (
     merge_configs,
 )
 from eodag.plugins.apis.base import Api
-from eodag.plugins.apis.ecmwf_group import EcmwfGroupApi
 from eodag.plugins.authentication.base import Authentication
 from eodag.plugins.base import EODAGPluginMount
 from eodag.plugins.crunch.base import Crunch
@@ -254,7 +253,7 @@ class PluginManager:
 
     def get_auth_plugin(
         self, associated_plugin: PluginTopic, product: Optional[EOProduct] = None
-    ) -> Union[Authentication, EcmwfGroupApi, None]:
+    ) -> Optional[Authentication]:
         """Build and return the authentication plugin associated to the given
         search/download plugin
 
@@ -283,7 +282,7 @@ class PluginManager:
                 self.get_auth_plugins(
                     associated_plugin.provider,
                     matching_url=matching_url,
-                    match_plugin=associated_plugin,
+                    matching_conf=associated_plugin.config,
                 )
             )
         except StopIteration:
@@ -294,25 +293,17 @@ class PluginManager:
         self,
         provider: str,
         matching_url: Optional[str] = None,
-        match_plugin: Optional[PluginTopic] = None,
-    ) -> Iterator[Union[Authentication, EcmwfGroupApi]]:
+        matching_conf: Optional[PluginConfig] = None,
+    ) -> Iterator[Authentication]:
         """Build and return the authentication plugin for the given product_type and
         provider
 
         :param provider: The provider for which to get the authentication plugin
         :param matching_url: url to compare with plugin matching_url pattern
-        :param match_plugin: plugin to compare with auth plugin matching_conf;
-                             if match_plugin is api plugin with authentification, this plugin is returned
+        :param matching_conf: configuration to compare with plugin matching_conf
         :returns: All the Authentication plugins for the given criteria
         """
         auth_conf: Optional[PluginConfig] = None
-        if isinstance(match_plugin, EcmwfGroupApi):
-            yield match_plugin
-
-        if match_plugin:
-            matching_conf = match_plugin.config
-        else:
-            matching_conf = None
 
         def _is_auth_plugin_matching(
             auth_conf: PluginConfig,
@@ -376,18 +367,17 @@ class PluginManager:
         self,
         provider: str,
         matching_url: Optional[str] = None,
-        match_plugin: Optional[PluginTopic] = None,
+        matching_conf: Optional[PluginConfig] = None,
     ) -> Optional[Union[AuthBase, S3SessionKwargs]]:
         """Authenticate and return the authenticated object for the first matching
         authentication plugin
 
         :param provider: The provider for which to get the authentication plugin
         :param matching_url: url to compare with plugin matching_url pattern
-        :param match_plugin: plugin to compare with auth plugin matching_conf;
-                             if match_plugin is api plugin with authentification, this plugin is returned
+        :param matching_conf: configuration to compare with plugin matching_conf
         :returns: All the Authentication plugins for the given criteria
         """
-        for auth_plugin in self.get_auth_plugins(provider, matching_url, match_plugin):
+        for auth_plugin in self.get_auth_plugins(provider, matching_url, matching_conf):
             if auth_plugin and callable(getattr(auth_plugin, "authenticate", None)):
                 try:
                     auth = auth_plugin.authenticate()
