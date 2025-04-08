991fe7c6a0bea939f564346d84060b04c79a684c
Sylvain Brunato <sylvain.brunato@c-s.fr>
2025-04-08 15:53:29 +0200

    test: ecmwf dates format

--- tests/context.py
+++ tests/context.py
@@ -72,6 +72,7 @@ from eodag.plugins.download.http import HTTPDownload
 from eodag.plugins.manager import PluginManager
 from eodag.plugins.search import PreparedSearch
 from eodag.plugins.search.base import Search
+from eodag.plugins.search.build_search_result import ecmwf_temporal_to_eodag
 from eodag.plugins.search.qssearch import QueryStringSearch
 from eodag.types import model_fields_to_annotated
 from eodag.types.queryables import CommonQueryables, Queryables
