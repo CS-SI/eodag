fbe4f6969c037efa42aec5d64f974372dfb63719
Sylvain Brunato <sylvain.brunato@c-s.fr>
2025-05-14 19:26:17 +0200

    fix: do not search for orderable id

--- eodag/rest/core.py
+++ eodag/rest/core.py
@@ -247,8 +247,6 @@ def download_stac_item(
     :returns: a stream of the downloaded data (zip file)
     """
     product_type = collection_id
-    if "ORDERABLE" in item_id:
-        item_id = ""
 
     search_results = eodag_api.search(
         id=item_id, productType=product_type, provider=provider, **kwargs
