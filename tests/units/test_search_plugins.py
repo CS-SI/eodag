0715aaa3c414de9568b6e977a52e36bff5d29ae3
Augustin Nesson <augustin.nesson@csgroup.eu>
2025-06-02 19:55:33 +0200

    fix: use 'on' instead of 'with'

--- tests/units/test_search_plugins.py
+++ tests/units/test_search_plugins.py
@@ -360,7 +360,7 @@ class TestSearchPluginQueryStringSearch(BaseSearchPluginTest):
             )
         self.assertEqual(
             "Search parameters which are not queryable are disallowed for "
-            f"{self.search_criteria_s2_msi_l1c['productType']} with {self.peps_search_plugin.provider}: "
+            f"{self.search_criteria_s2_msi_l1c['productType']} on {self.peps_search_plugin.provider}: "
             "please remove 'foo' from your search parameters",
             context.exception.message,
         )
@@ -388,7 +388,7 @@ class TestSearchPluginQueryStringSearch(BaseSearchPluginTest):
             )
         self.assertEqual(
             "Search parameters which are not queryable are disallowed for "
-            f"{self.search_criteria_s2_msi_l1c['productType']} with {self.peps_search_plugin.provider}: "
+            f"{self.search_criteria_s2_msi_l1c['productType']} on {self.peps_search_plugin.provider}: "
             "please remove 'foo' from your search parameters",
             context.exception.message,
         )
@@ -3676,7 +3676,7 @@ class TestSearchPluginPostJsonSearchWithStacQueryables(BaseSearchPluginTest):
             )
         self.assertEqual(
             "Search parameters which are not queryable are disallowed for "
-            f"{self.search_criteria_s2_msi_l1c['productType']} with {self.wekeomain_search_plugin.provider}: "
+            f"{self.search_criteria_s2_msi_l1c['productType']} on {self.wekeomain_search_plugin.provider}: "
             "please remove 'foo' from your search parameters",
             context.exception.message,
         )
@@ -3704,7 +3704,7 @@ class TestSearchPluginPostJsonSearchWithStacQueryables(BaseSearchPluginTest):
             )
         self.assertEqual(
             "Search parameters which are not queryable are disallowed for "
-            f"{self.search_criteria_s2_msi_l1c['productType']} with {self.wekeomain_search_plugin.provider}: "
+            f"{self.search_criteria_s2_msi_l1c['productType']} on {self.wekeomain_search_plugin.provider}: "
             "please remove 'foo' from your search parameters",
             context.exception.message,
         )
