8f03fba839dd25cbd55a97b3a7343d5afcde680c
Aubin Lambaré <aubin.lambare@cs-soprasteria.com>
2025-06-20 17:27:19 +0200

    chore: update copyright

--- eodag/cache.py
+++ eodag/cache.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2022, CS GROUP - France, https://www.csgroup.eu/
+# Copyright 2025, CS GROUP - France, https://www.cs-soprasteria.com/
 #
 # This file is part of EODAG project
 #     https://www.github.com/CS-SI/EODAG
