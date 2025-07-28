cc95c21a449b9d4f89c7aba448d12fed5900eb62
jlahovnik <julia.lahovnik@csgroup.eu>
2025-07-28 17:50:46 +0200

    style: import order

--- eodag/plugins/download/creodias_s3.py
+++ eodag/plugins/download/creodias_s3.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional, Union, TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, Union
 
 import boto3
 from botocore.exceptions import ClientError
