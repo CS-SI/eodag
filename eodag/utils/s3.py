051a51674174c70a3344416a1abd1805ec58127b
Guillermo Tesoro Calvo <guillermo.tesoro@c-ssystems.de>
2025-09-11 16:06:43 +0200

    Normalized asset keys are now propagated to the title

--- eodag/utils/s3.py
+++ eodag/utils/s3.py
@@ -24,7 +24,6 @@ import uuid
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import TYPE_CHECKING
-from urllib.parse import urlparse
 from zipfile import ZIP_STORED, ZipFile
 
 import boto3
@@ -541,12 +540,10 @@ def update_assets_from_s3(
             key, roles = product.driver.guess_asset_key_and_roles(
                 out_of_zip_url, product
             )
-            parsed_url = urlparse(out_of_zip_url)
-            title = os.path.basename(parsed_url.path)
 
             if key and key not in product.assets:
                 product.assets[key] = {
-                    "title": title,
+                    "title": key,  # Normalize title with key
                     "roles": roles,
                     "href": asset_url,
                 }
