ac357579c683df4683c4b6fec51953779e037e68
Sylvain Brunato <sylvain.brunato@c-s.fr>
2025-11-03 15:43:32 +0100

    ci: pre-commit replaced with faster prek

--- eodag/config.py
+++ eodag/config.py
@@ -241,7 +241,8 @@ class PluginConfig(yaml.YAMLObject):
         next_page_url_tpl: str
         #: The query-object for POST pagination requests.
         next_page_query_obj: str
-        #: Next page token key used in pagination
+        #: Next page token key used in pagination. Can be guessed from ``KNOWN_NEXT_PAGE_TOKEN_KEYS`` (but needed by
+        # ``stac-fastapi-eodag`` that cannot guess and will use ``page`` as default).
         next_page_token_key: str
         #: The endpoint for counting the number of items satisfying a request
         count_endpoint: str
