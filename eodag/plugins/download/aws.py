5400ded74bb020f74d4de9cca7460e4a65b05d68
Augustin Nesson <augustin.nesson@csgroup.eu>
2025-12-11 18:21:41 +0100

    fix: check S3ServiceResource type in a correct way

--- eodag/plugins/download/aws.py
+++ eodag/plugins/download/aws.py
@@ -771,7 +771,10 @@ class AwsDownload(Download):
             ignore_assets,
             product,
         )
-        if auth and isinstance(auth, boto3.resource("s3").__class__):
+
+        # check if auth is a S3 resource by verifying it has the meta.client attribute.
+        # isinstance() check is not possible here because it is a dynamically generated class.
+        if auth and hasattr(auth, "meta") and hasattr(auth.meta, "client"):
             s3_resource = auth
         else:
             s3_resource = boto3.resource(
