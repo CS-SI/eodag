f170e0f970a9b8d7d2dc1bb78c95ae190bc3c87b
cauriol <camille.auriol@cs-soprasteria.com>
2026-02-18 16:02:05 +0100

    fix: precomit

--- tests/units/test_core.py
+++ tests/units/test_core.py
@@ -618,7 +618,7 @@ class TestCore(TestCoreBase):
         "SPACENET_BUILDINGS_DETECTION_V1": ["dedl"],
         "SPACENET_ALL_WEATHER_MAPPING": ["dedl"],
         "SPACENET_BUILDINGS_DETECTION_V2": ["dedl"],
-        "SPACENET_ROADS_NETWORK_DETECTION": ["dedl"], 
+        "SPACENET_ROADS_NETWORK_DETECTION": ["dedl"],
         "SPACENET_OFF_NADIR_BUILDING": ["dedl"],
         "SPACENET_ROADS_NETWORK_ROUTE_TRAVEL": ["dedl"],
         GENERIC_COLLECTION: [
