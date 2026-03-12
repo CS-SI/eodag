de7562e7ccc5e4da2af9a89af9e565a6d556ce5f
Augustin Nesson <augustin.nesson@csgroup.eu>
2026-03-12 17:21:33 +0100

    fix: convert SQLite database model to a class
    add an interface called 'Database'

--- eodag/api/core.py
+++ eodag/api/core.py
@@ -89,6 +89,7 @@ if TYPE_CHECKING:
     from concurrent.futures import ThreadPoolExecutor
     from shapely.geometry.base import BaseGeometry
 
+    from eodag.databases.base import Database
     from eodag.plugins.apis.base import Api
     from eodag.plugins.crunch.base import Crunch
     from eodag.plugins.search.base import Search
@@ -111,10 +112,10 @@ class EODataAccessGateway:
         self,
         user_conf_file_path: Optional[str] = None,
         locations_conf_path: Optional[str] = None,
+        db: Optional[Database] = None,
     ) -> None:
         # handle database initialization
-        self.db = SQLiteDatabase.create_with_dag(self)
-        self.db.prepare_database()
+        self.db = db if db is not None else SQLiteDatabase.create_with_dag(self)
 
         collections_config_path = os.getenv("EODAG_COLLECTIONS_CFG_FILE") or str(
             res_files("eodag") / "resources" / "collections.yml"
---
+++ eodag/databases/base.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Copyright 2026, CS GROUP - France, https://www.csgroup.eu/
+#
+# This file is part of EODAG project
+#     https://www.github.com/CS-SI/EODAG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Optional, Union
+
+if TYPE_CHECKING:
+    from shapely.geometry.base import BaseGeometry
+
+    from eodag.api.collection import Collection, CollectionsDict
+    from eodag.api.provider import ProviderConfig
+
+
+class Database:
+    """Base for classes representing a database.
+
+    A Database object is used to store information about :class:`~eodag.api.collection.Collection` objects.
+    """
+
+    type: str
+
+    def _create_col_table(self) -> None:
+        """Create the collections table in the database."""
+        raise NotImplementedError
+
+    def _create_col_config_table(self) -> None:
+        """Create the collections configuration table in the database."""
+        raise NotImplementedError
+
+    def get_collection(self, collection_name: str) -> Optional[Collection]:
+        """Retrieve a collection from the database."""
+        raise NotImplementedError
+
+    def get_collection_from_alias(self, alias_or_id: str) -> Collection:
+        """Retrieve a collection from the database by either its id or alias.
+
+        :param alias_or_id: Alias of the collection. If an existing id is given, this
+                            method will directly return the collection of given value.
+        :returns: The collection having  Internal name of the collection.
+        """
+        raise NotImplementedError
+
+    def delete_collection(self, collection_name: str) -> None:
+        """Delete a collection from the database."""
+        raise NotImplementedError
+
+    def rename_collection(self, old_name: str, new_name: str) -> None:
+        """Rename a collection in the database."""
+        raise NotImplementedError
+
+    def upsert_collections(self, collections: CollectionsDict) -> None:
+        """Add or update collections in the database"""
+        raise NotImplementedError
+
+    def upsert_providers_config(self, providers_config: list[ProviderConfig]) -> None:
+        """Add or update providers configurations in the database"""
+        raise NotImplementedError
+
+    def search_collections(
+        self,
+        geometry: Optional[Union[str, dict[str, float], BaseGeometry]] = None,
+        datetime: Optional[str] = None,
+        limit: Optional[int] = 10,
+        q: Optional[list[str]] = None,  # Free-Text Search
+        query: Optional[str] = None,  # STACQL
+        filter_: Optional[dict[str, Any]] = None,  # cql2
+        count: bool = False,
+    ) -> tuple[CollectionsDict, Optional[int]]:
+        """
+        :returns: Collections matching all the given parameters.
+        """
+        raise NotImplementedError
