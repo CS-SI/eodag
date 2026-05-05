232fb7dfa823c560710cb52a9cc4b84ab50ac1ba
jlahovnik <julia.lahovnik@csgroup.eu>
2026-05-05 09:31:00 +0200

    fix: cop_marine asset urls

--- eodag/plugins/search/cop_marine.py
+++ eodag/plugins/search/cop_marine.py
@@ -192,7 +192,11 @@ class CopMarineSearch(StaticStacSearch):
         item_id = os.path.splitext(item_key.split("/")[-1])[0]
         if product_id and product_id != item_id:
             return None
-        download_url = s3_url + "/" + item_key
+        s3_url_parts = s3_url.split("/")
+        item_key_parts = [
+            part for part in item_key.split("/") if part not in s3_url_parts
+        ]
+        download_url = s3_url + "/" + "/".join(item_key_parts)
         geometry = (
             get_geometry_from_various(**dataset_item)
             or self.config.metadata_mapping["eodag:default_geometry"]
