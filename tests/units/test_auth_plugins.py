b01f5787a2e47a70a4003dd8c0bad6a41c16f818
Augustin Nesson <augustin.nesson@csgroup.eu>
2026-07-29 15:37:44 +0200

    test: fix some tests

--- tests/units/test_auth_plugins.py
+++ tests/units/test_auth_plugins.py
@@ -33,7 +33,6 @@ from requests.auth import AuthBase
 from requests.exceptions import RequestException
 
 from eodag.api.product._product import EOProduct
-from eodag.api.provider import ProvidersDict
 from eodag.plugins.authentication.eoiam import _EOIAMSessionAuth
 from eodag.utils import MockResponse
 from tests.context import (
@@ -948,7 +947,7 @@ class TestAuthPluginEOIAMAuth(BaseAuthPluginTest):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        providers = ProvidersDict.from_configs(
+        providers = build_provider_configs(
             {
                 "foo_provider": {
                     "products": {"foo_product": {}},
@@ -959,7 +958,7 @@ class TestAuthPluginEOIAMAuth(BaseAuthPluginTest):
                 },
             }
         )
-        cls.plugins_manager = PluginManager(providers)
+        cls.plugins_manager = make_plugins_manager(providers)
 
     def test_plugins_auth_eoiam_validate_credentials_empty(self):
         """EOIAMAuth.validate_config_credentials must raise an error on empty credentials"""
--- tests/units/test_search_plugins.py
+++ tests/units/test_search_plugins.py
@@ -80,6 +80,7 @@ from tests.context import (
     NotAvailableError,
     PluginConfig,
     PreparedSearch,
+    ProviderConfig,
     QueryablesDict,
     QueryStringSearch,
     RequestError,
@@ -368,7 +369,8 @@ class TestSearchPluginQueryStringSearchXml(BaseSearchPluginTest):
         provider_name, mundi_config = next(iter(mundi_config_dict.items()))
         if "name" not in mundi_config:
             mundi_config["name"] = provider_name
-        add_provider_to_pm(self.plugins_manager, mundi_config)
+        mundi_config_obj = ProviderConfig.from_mapping(mundi_config)
+        add_provider_to_pm(self.plugins_manager, mundi_config_obj)
 
         # One of the providers that has a QueryStringSearch Search plugin and result_type=xml
         self.mundi_search_plugin = self.get_search_plugin(self.collection, provider)
@@ -2014,7 +2016,8 @@ class TestSearchPluginODataV4Search(BaseSearchPluginTest):
         provider_name, onda_config = next(iter(onda_config_dict.items()))
         if "name" not in onda_config:
             onda_config["name"] = provider_name
-        add_provider_to_pm(self.plugins_manager, onda_config)
+        onda_config_obj = ProviderConfig.from_mapping(onda_config)
+        add_provider_to_pm(self.plugins_manager, onda_config_obj)
 
         # One of the providers that has a ODataV4Search Search plugin
         provider = "onda"
@@ -7062,7 +7065,7 @@ class TestSearchPluginCopGhslSearch(BaseSearchPluginTest):
     def test_plugins_search_cop_ghsl_discover_queryables(self, mock_fetch_constraints):
         """test that the correct queryables are returned"""
         mock_fetch_constraints.return_value = {"constraints": self.constraints}
-        plugin = self.get_search_plugin(collection="GHS_BUILT_S", provider="cop_ghsl")
+        plugin = self.get_search_plugin(provider="cop_ghsl")
         kwargs = {"collection": "GHS_BUILT_S"}
         collection_config = {
             key: value
